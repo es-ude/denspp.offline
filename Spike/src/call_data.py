ebbdd16e196506d365b430be32a6609346209baa
Mahya <mahya.latifian@stud.uni-due.de>
2022-11-13 21:27:36 +0100

    add resampling function.

--- Spike/src/call_data.py
+++ Spike/src/call_data.py
@@ -1,7 +1,12 @@
 import numpy as np
 import os
+from typing import Tuple
+from fractions import Fraction
+
 from scipy.io import loadmat
+from scipy.signal import resample_poly
 import mat73
+
 
 # ----- Read Settings -----
 class Labeling:
@@ -12,10 +17,10 @@ class Labeling:
     orig_spike_no = None
 
 
-class Neuro_input:
+class NeuroInput:
     type = None
     gain = None
-    origFs = None
+    orig_fs = None
     channel = None
     fs = None
     time = None
@@ -25,26 +30,25 @@ class Neuro_input:
 
 def call_data(path2data: str, data_type: int, data_set: int, desired_fs: int, t_range: np.array):
     labeling = Labeling()
-    neuron = Neuro_input()
+    neuron = NeuroInput()
 
     # ----- Read data input -----
-    match data_type:
-        case 1:
-            (data_in, label_in) = load_01_SimDaten_Martinez2009(path2data, data_set)
-        case 2:
-            (data_in, label_in) = load_02_SimDaten_Pedreira2012(path2data, data_set)
-        case 3:
-            print("C")
-        case 4:
-            print("D")
+    if data_type == 1:
+        (data_in, label_in) = load_01_SimDaten_Martinez2009(path2data, data_set)
+    elif data_type == 2:
+        (data_in, label_in) = load_02_SimDaten_Pedreira2012(path2data, data_set)
+    elif data_type == 3:
+        print("C")
+    elif data_type == 4:
+        print("D")
 
     # --- Processing of raw data
     neuron.type = data_in["type"]
     neuron.channel = data_in["channel"]
-    neuron.origFs = data_in["sampling_rate"]
+    neuron.orig_fs = data_in["sampling_rate"]
     neuron.gain_pre = data_in["gain"]
     neuron.rawdata = data_in["raw_data"] / neuron.gain_pre
-    t0 = np.arange(0, neuron.rawdata.size - 1, 1).reshape((1, neuron.rawdata.size - 1)) / neuron.origFs
+    t0 = np.arange(0, neuron.rawdata.size, 1).reshape((1, -1)) / neuron.orig_fs
 
     # --- Processing of labeling informations
     spike_class = label_in["spike_cluster"]
@@ -63,15 +67,44 @@ def call_data(path2data: str, data_type: int, data_set: int, desired_fs: int, t_
         idx1 = np.argwhere(t_range[0, 1] <= t0)
         idx1 = int(idx1[0, 1])
 
-        neuron.time = np.linspace(0, idx1 - idx0 - 1, idx1 - idx0) / neuron.orig_fs
-        neuron.data = neuron.rawdata[idx0:idx1]
+        t0 = t0[[0], idx0:idx1]
+        neuron.rawdata = neuron.rawdata[[0], idx0:idx1]
     else:
         neuron.time = t0
-        neuron.data = neuron.data
+        neuron.rawdata = neuron.rawdata
 
     # TODO: Neuabtastung einfügen
+    len_data_cut = t0.size
+    if desired_fs != neuron.orig_fs and desired_fs != 0:
+        t = np.zeros(shape=t0.shape)
+        u_safe = 5e-6
+        if np.abs(np.sum((np.mean(neuron.rawdata[0]) - np.array([-1, 1]) * u_safe) < 0) - 1) == 1:
+            du = np.mean(neuron.rawdata[0])
+        else:
+            du = 0
+
+        p, q = get_resample_ratio(t0, desired_fs)
+
+        neuron.data = resample_poly(neuron.rawdata[0] - du, p, q)
+        neuron.data = (neuron.data + du).reshape((1, -1))
+        neuron.time = resample_poly(t0[0], p, q).reshape((1, -1))
+        neuron.fs = desired_fs
+
+    else:
+        neuron.data = neuron.rawdata
+        neuron.time = t0
+        neuron.fs = neuron.orig_fs
 
     return (neuron, labeling)
+
+
+def get_resample_ratio(t, fs) -> Tuple[int, int]:
+    # average sampling interval
+    calced_fs = ((t[0, -1] - t[0, 0]) / (t.size)) * fs
+    # matlab 'rat' function
+    p, q = Fraction(calced_fs).limit_denominator(100).as_integer_ratio()
+
+    return p, q
 
 
 def load_01_SimDaten_Martinez2009(path2data: str, indices: list = [1, 2, 3, 4, 5]):
@@ -87,7 +120,7 @@ def load_01_SimDaten_Martinez2009(path2data: str, indices: list = [1, 2, 3, 4, 5
     data["channel"] = int(loaded_data["chan"][0])
     data["gain"] = 10 ** (0 / 20)
     data["sampling_rate"] = int(1 / loaded_data["samplingInterval"][0][0] * 1000)
-    data["raw_data"] = 0.5e-6 * loaded_data["data"][0]
+    data["raw_data"] = 0.5e-6 * loaded_data["data"] / data["gain"]
 
     label["spike_times"] = loaded_data["spike_times"][0][0][0]
     label["spike_cluster"] = loaded_data["spike_class"][0][0][0]
