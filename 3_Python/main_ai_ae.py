Bug-fixed errors in TensorBoard, SNR calculation in AE Training, Plot errors and getting more results in KFold Training

3_Python/main_ai_ae.py
@@ -7,24 +7,26 @@ from package.plotting.plot_dnn import results_training, plot_statistic_data
 from package.dnn.pytorch_control import Config_PyTorch
 from package.dnn.pytorch_autoencoder import *
 from package.dnn.dataset.autoencoder import prepare_plotting, prepare_training
-from package.dnn.data_preprocessing import calculate_frame_mean, calculate_frame_snr
 import package.dnn.models.autoencoder as ai_module
 
 
+noise_std = 1
 # 0 = normal autoencoder, 1 = denoising AE (mean), 2 = denoising AE (more noise input)
-mode_train = 0
+mode_train = 2
 
 config_train = Config_PyTorch(
     # --- Settings of Models/Training
-    model=ai_module.dnn_ae_rgc_fzj_v2(),
+    model=ai_module.dnn_ae_v2(),
     loss_fn=nn.MSELoss(),
     optimizer='Adam',
     num_kfold=1,
-    num_epochs=3,
+    num_epochs=100,
     batch_size=256,
     # --- Settings of Datasets
-    data_path='data',
-    data_file_name='2023-11-16_rgc_onoff_fzj.mat',
+    #data_path='data',
+    #data_file_name='2023-11-16_rgc_onoff_fzj.mat',
+    data_path='../2_Data/00_Merged_Datasets',
+    data_file_name='2023-05-15_Dataset01_SimDaten_Martinez2009_Sorted.mat',
     data_split_ratio=0.25,
     data_do_shuffle=True,
     # --- Data Augmentation
@@ -32,10 +34,10 @@ config_train = Config_PyTorch(
     data_num_augmentation=0,
     data_do_normalization=False,
     data_do_addnoise_cluster=False,
-    data_do_reduce_samples_per_cluster=True,
+    data_do_reduce_samples_per_cluster=False,
     data_num_samples_per_cluster=20000,
     # --- Dataset Preparation
-    data_exclude_cluster=[],
+    data_exclude_cluster=[4],
     data_sel_pos=[]
 )
 
@@ -45,50 +47,54 @@ if __name__ == "__main__":
     print("\nTrain modules of spike-sorting frame-work (MERCUR-project Sp:AI:ke, 2022-2024)")
 
     # --- Processing: Loading dataset and Do Training
-    dataset = prepare_training(path=config_train.get_path2data(), settings=config_train, mode_train_ae=mode_train)
+    dataset = prepare_training(path=config_train.get_path2data(), settings=config_train,
+                               mode_train_ae=mode_train, do_classification=False,
+                               noise_std=noise_std)
     dataset_dict = dataset.frame_dict
+    data_mean = dataset.frames_me
     trainhandler = pytorch_train(config_train)
     trainhandler.load_model()
     trainhandler.load_data(dataset)
     del dataset
-    loss, snr_train = trainhandler.do_training()
+    snr_train = trainhandler.do_training()[1]
 
     # --- Post-Processing: Getting data from validation set for inference
-    data_in, data_out, cluster_out, data_mean = prepare_plotting(trainhandler.valid_loader)
-    yclus = prepare_plotting(trainhandler.train_loader)[2]
+    take_fold = 0
+    data_in, data_out, cluster_out, data_mean0 = prepare_plotting(trainhandler.valid_loader[take_fold])
+    yclus = prepare_plotting(trainhandler.train_loader[take_fold])[2]
 
     # --- Post-Processing: Do the Inference with Best Model
     print(f"\nDoing the inference with validation data on best model")
-    model_test = load(trainhandler.get_best_model()[0])
+    model_test = load(trainhandler.get_best_model()[take_fold])
     feat_out, pred_out = model_test(from_numpy(data_in))
-    feat0 = feat_out.detach().numpy()
-    ypred0 = pred_out.detach().numpy()
+    feat_out = feat_out.detach().numpy()
+    pred_out = pred_out.detach().numpy()
 
     # --- Post-Processing: Calculating the improved SNR
     snr_in = []
     snr_out = []
     for i, frame in enumerate(data_in):
-        snr_in.append(calculate_snr(frame, data_mean[i, :]))
-        snr_out.append(calculate_snr(ypred0[i, :], data_mean[i, :]))
+        snr_in.append(calculate_snr(frame, data_mean0[i, :]))
+        snr_out.append(calculate_snr(pred_out[i, :], data_mean0[i, :]))
+    del i, frame, model_test
 
     snr_in = np.array(snr_in)
     snr_out = np.array(snr_out)
     snr_delta = snr_out - snr_in
 
-    print(f"- SNR_in: {np.mean(snr_in):.2f} (mean) | {np.max(snr_in):.2f} (max) | {np.min(snr_in):.2f} (min)")
-    print(f"- SNR_out: {np.mean(snr_out):.2f} (mean) | {np.max(snr_out):.2f} (max) | {np.min(snr_out):.2f} (min)")
-    print(f"- SNR_inc: {np.mean(snr_delta): .2f} (mean)")
+    print(f"\nCalcuted SNR values from inference on validated datas")
+    print(f"- SNR_in: {np.median(snr_in):.2f} (median) | {np.max(snr_in):.2f} (max) | {np.min(snr_in):.2f} (min)")
+    print(f"- SNR_out: {np.median(snr_out):.2f} (median) | {np.max(snr_out):.2f} (max) | {np.min(snr_out):.2f} (min)")
+    print(f"- SNR_inc: {np.median(snr_delta): .2f} (median)")
 
-    # --- Reducing data_mean and Saving data
-    data_mean = calculate_frame_mean(data_in, cluster_out)
-
+    # --- Saving data
     logsdir = trainhandler.get_saving_path()
     savemat(join(logsdir, 'results.mat'),
             {"frames_in": data_in,
              "frames_out": data_out,
              "frames_mean": data_mean,
-             "frames_pred": ypred0,
-             "feat": feat0,
+             "frames_pred": pred_out,
+             "feat": feat_out,
              "cluster": cluster_out,
              "config": config_train},
             do_compression=True,
@@ -96,14 +102,11 @@ if __name__ == "__main__":
 
     # --- Plotting
     results_training(
-        path=logsdir, feat=feat0,
-        yin=data_in, ypred=ypred0, ymean=data_mean,
+        path=logsdir, feat=feat_out,
+        yin=data_in, ypred=pred_out, ymean=data_mean,
         cluster=cluster_out, snr=snr_train
     )
     plot_statistic_data(yclus, cluster_out, path2save=logsdir, cl_dict=dataset_dict)
 
     plt.show(block=False)
     plt.close("all")
-
-    print("\nLook data on TensorBoard -> open Terminal")
-    print("Type in: tensorboard serve --logdir ./runs")

3_Python/package/dnn/data_preprocessing.py
@@ -41,13 +41,30 @@ def calculate_frame_mean(
     NoCluster, NumCluster = np.unique(frames_cl, return_counts=True)
     SizeCluster = np.size(NoCluster)
 
-    frames_mean = np.zeros(shape=(SizeCluster, frames_in.shape[1]), dtype=int)
+    frames_out = np.zeros(shape=(SizeCluster, frames_in.shape[1]))
     for idx0, val in enumerate(NoCluster):
         # --- Mean waveform
         indices = np.argwhere(frames_cl == val).flatten()
-        frames_mean[idx0, :] = np.mean(frames_in[indices, :], axis=0, dtype=int)
+        frames_out[idx0, :] = np.mean(frames_in[indices, :], axis=0)
 
-    return frames_mean
+    return frames_out.astype(int)
+
+
+def calculate_frame_median(
+        frames_in: np.ndarray,
+        frames_cl: np.ndarray
+    ) -> np.ndarray:
+    """Calculating mean waveforms of spike waveforms with median()"""
+    NoCluster, NumCluster = np.unique(frames_cl, return_counts=True)
+    SizeCluster = np.size(NoCluster)
+
+    frames_out = np.zeros(shape=(SizeCluster, frames_in.shape[1]))
+    for idx0, val in enumerate(NoCluster):
+        # --- Mean waveform
+        indices = np.argwhere(frames_cl == val).flatten()
+        frames_out[idx0, :] = np.median(frames_in[indices, :], axis=0)
+
+    return frames_out.astype(int)
 
 
 def calculate_frame_snr(

3_Python/package/dnn/models/autoencoder_embedded.py
@@ -15,7 +15,7 @@ class dnn_dae_v2(nn.Module):
         self.model_embedded = True
         bits_total = 12
         bits_frac = 9
-        iohiddenlayer = [self.model_shape[1], 20, output_size]
+        iohiddenlayer = [input_size, 20, output_size]
         do_train_bias = True
 
         # --- Encoder Path

3_Python/package/dnn/pytorch_classification.py
@@ -1,10 +1,9 @@
+import numpy as np
 from os import remove
 from os.path import join, exists
 from glob import glob
-import shutil
-import numpy as np
+from shutil import copy
 from datetime import datetime
-
 from torch import load, save
 from package.dnn.pytorch_control import Config_PyTorch, training_pytorch
 
@@ -64,6 +63,7 @@ class pytorch_train(training_pytorch):
         """Start model training incl. validation and custom-own metric calculation"""
         self._init_train()
         self._save_config_txt()
+
         # --- Handling Kfold cross validation training
         if self._do_kfold:
             print(f"Starting Kfold cross validation training in {self.settings.num_kfold} steps")
@@ -73,18 +73,17 @@ class pytorch_train(training_pytorch):
         path2model = str()
         path2model_init = join(self._path2save, f'model_reset.pth')
         save(self.model.state_dict(), path2model_init)
+        timestamp_start = datetime.now()
+        timestamp_string = timestamp_start.strftime('%H:%M:%S')
+
         for fold in np.arange(self.settings.num_kfold):
             # Reseting the model
             self.model.load_state_dict(load(path2model_init))
             self._run_kfold = fold
             self._init_writer()
 
-            timestamp_start = datetime.now()
-            timestamp_string = timestamp_start.strftime('%H:%M:%S')
-            if self._do_kfold:
-                print(f'\nTraining starts on: {timestamp_string} with fold #{fold}')
-            else:
-                print(f'\nTraining starts on: {timestamp_string}')
+            addon = f' with fold #{fold}' if self._do_kfold else ''
+            print(f'\nTraining starts on {timestamp_string}' + addon)
 
             best_loss = [1e6, 1e6]
             best_acc = [0.0, 0.0]
@@ -98,17 +97,17 @@ class pytorch_train(training_pytorch):
                       f'valid_loss = {valid_loss:.5f}, valid_acc = {100 * valid_acc:.2f} %')
 
                 # Log the running loss averaged per batch for both training and validation
-                self._writer.add_scalar('Loss_train', train_loss)
-                self._writer.add_scalar('Loss_valid', valid_loss)
-                self._writer.add_scalar('Acc_train', train_acc)
-                self._writer.add_scalar('Acc_valid', valid_acc, epoch+1)
+                self._writer.add_scalar('Loss_train (CL)', train_loss, epoch+1)
+                self._writer.add_scalar('Loss_valid (CL)', valid_loss, epoch+1)
+                self._writer.add_scalar('Acc_train (CL)', train_acc, epoch+1)
+                self._writer.add_scalar('Acc_valid (CL)', valid_acc, epoch+1)
                 self._writer.flush()
 
                 # Tracking the best performance and saving the model
                 if valid_loss < best_loss[1]:
                     best_loss = [train_loss, valid_loss]
                     best_acc = [train_acc, valid_acc]
-                    path2model = join(self._path2log, f'model_fold{fold:03d}_epoch{epoch:04d}.pth')
+                    path2model = join(self._path2temp, f'model_fold{fold:03d}_epoch{epoch:04d}.pth')
                     save(self.model, path2model)
 
                 # Saving metrics
@@ -116,27 +115,11 @@ class pytorch_train(training_pytorch):
 
             # --- Ausgabe nach Training
             metrics.append(best_loss)
+            copy(path2model, self._path2save)
             self._save_train_results(best_loss[0], best_loss[1], 'Loss')
             self._save_train_results(best_acc[0], best_acc[1], 'Acc.')
 
-            timestamp_end = datetime.now()
-            timestamp_string = timestamp_end.strftime('%H:%M:%S')
-            diff_time = timestamp_end - timestamp_start
-            diff_string = diff_time
-
-            print(f'Training ends on: {timestamp_string}')
-            print(f'Training runs: {diff_string}')
-            print(f'Save best model: {path2model}')
-            shutil.copy(path2model, self._path2save)
-
         # --- Ending of all trainings phases
-        # Delete init model
-        if exists(path2model_init):
-            remove(path2model_init)
-
-        # Delete log folders
-        folder_logs = glob(join(self._path2save, 'logs*'))
-        for folder in folder_logs:
-            shutil.rmtree(folder, ignore_errors=True)
+        self._end_training_routine(timestamp_start)
 
         return metrics, own_metric

3_Python/package/dnn/pytorch_control.py
@@ -1,10 +1,11 @@
 import dataclasses
-from typing import Any
-from os import mkdir
-from os.path import exists, join
-from glob import glob
 import platform
 import numpy as np
+from typing import Any
+from os import mkdir, remove
+from os.path import exists, join
+from shutil import rmtree
+from glob import glob
 from datetime import datetime
 from torch import optim, device, cuda
 from torch.utils.tensorboard import SummaryWriter
@@ -74,7 +75,10 @@ class training_pytorch:
 
         # --- Preparing options
         self._do_kfold = False
+        self._do_shuffle = config_train.data_do_shuffle
         self._run_kfold = 0
+        self._samples_train = list()
+        self._samples_valid = list()
 
         # --- Saving options
         self._index_folder = 'train' if do_train else 'inference'
@@ -83,6 +87,7 @@ class training_pytorch:
         self._model_addon = str()
         self._path2run = 'runs'
         self._path2log = str()
+        self._path2temp = str()
         self._path2config = str()
         self.config_available = False
         self._path2save = str()
@@ -112,44 +117,49 @@ class training_pytorch:
         """Do init of class for training"""
         folder_name = f'{datetime.now().strftime("%Y%m%d_%H%M%S")}_{self._index_folder}_{self._model_name}'
         self._path2save = join(self._path2run, folder_name)
+        self._path2temp = join(self._path2save, f'temp')
 
         if not exists(self._path2run):
             mkdir(self._path2run)
 
         mkdir(self._path2save)
+        mkdir(self._path2temp)
 
     def _init_writer(self) -> None:
         """Do init of writer"""
-        self._path2log = join(self._path2save, f'logs_{self._run_kfold:03d}')
-        self._writer = SummaryWriter(self._path2log)
+        self._path2log = join(self._path2save, f'logs')
+        self._writer = SummaryWriter(self._path2log, comment=f"event_log_kfold{self._run_kfold:03d}")
 
     def load_data(self, data_set) -> None:
         """Loading data for training and validation in DataLoader format into class"""
         self._do_kfold = True if self.settings.num_kfold > 1 else False
-        num_samples = len(data_set)
         self._model_addon = data_set.data_type
 
         # --- Preparing datasets
         out_train = list()
         out_valid = list()
         if self._do_kfold:
-            kfold = KFold(n_splits=self.settings.num_kfold, shuffle=True)
-            for fold, (idx_train, idx_valid) in enumerate(kfold.split(np.arange(num_samples))):
+            kfold = KFold(n_splits=self.settings.num_kfold, shuffle=self._do_shuffle)
+            for idx_train, idx_valid in kfold.split(np.arange(len(data_set))):
                 subsamps_train = SubsetRandomSampler(idx_train)
                 subsamps_valid = SubsetRandomSampler(idx_valid)
                 out_train.append(DataLoader(data_set, batch_size=self.settings.batch_size, sampler=subsamps_train))
                 out_valid.append(DataLoader(data_set, batch_size=self.settings.batch_size, sampler=subsamps_valid))
+                self._samples_train.append(subsamps_train.indices.size)
+                self._samples_valid.append(subsamps_valid.indices.size)
         else:
-            idx = np.arange(num_samples)
-            np.random.shuffle(idx)
-            pos = int(num_samples * (1 - self.settings.data_split_ratio))
-            idx_train = idx[0:pos]
-            idx_valid = idx[pos:]
+            idx = np.arange(len(data_set))
+            if self._do_shuffle:
+                np.random.shuffle(idx)
+            split_pos = int(len(data_set) * (1 - self.settings.data_split_ratio))
+            idx_train = idx[0:split_pos]
+            idx_valid = idx[split_pos:]
             subsamps_train = SubsetRandomSampler(idx_train)
             subsamps_valid = SubsetRandomSampler(idx_valid)
-
             out_train.append(DataLoader(data_set, batch_size=self.settings.batch_size, sampler=subsamps_train))
             out_valid.append(DataLoader(data_set, batch_size=self.settings.batch_size, sampler=subsamps_valid))
+            self._samples_train.append(subsamps_train.indices.size)
+            self._samples_valid.append(subsamps_valid.indices.size)
 
         # --- Output
         self.train_loader = out_train
@@ -192,9 +202,9 @@ class training_pytorch:
         """Writing some training metrics into txt-file"""
         if self.config_available:
             with open(self._path2config, 'a') as txt_handler:
-                txt_handler.write('\n--- Results of last epoch ---')
+                txt_handler.write(f'\n--- Metrics of last epoch in fold #{self._run_kfold} ---')
                 txt_handler.write(f'\nTraining {type} = {last_metric_train}')
-                txt_handler.write(f'\nValidation {type} = {last_metric_valid}')
+                txt_handler.write(f'\nValidation {type} = {last_metric_valid}\n')
 
     def get_saving_path(self) -> str:
         """Getting the path for saving files in aim folder"""
@@ -204,3 +214,27 @@ class training_pytorch:
         """Getting the path to the best trained model"""
         return glob(join(self._path2save, "*.pth"))
 
+    def _end_training_routine(self, timestamp_start: datetime, do_delete_temps=True) -> None:
+        """Doing the last step of training routine"""
+        timestamp_end = datetime.now()
+        timestamp_string = timestamp_end.strftime('%H:%M:%S')
+        diff_time = timestamp_end - timestamp_start
+        diff_string = diff_time
+
+        print(f'\nTraining ends on: {timestamp_string}')
+        print(f'Training runs: {diff_string}')
+
+        # Delete init model
+        init_model = glob(join(self._path2save, 'model_reset.pth'))
+        for file in init_model:
+            remove(file)
+
+        # Delete log folders
+        if do_delete_temps:
+            folder_logs = glob(join(self._path2save, 'temp*'))
+            for folder in folder_logs:
+                rmtree(folder, ignore_errors=True)
+
+        # Give the option to open TensorBoard
+        print("\nLook data on TensorBoard -> open Terminal")
+        print("Type in: tensorboard serve --logdir ./runs")