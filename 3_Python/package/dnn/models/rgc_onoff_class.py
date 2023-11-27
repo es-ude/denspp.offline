05b9afd992fb2a1856e4123bbc6fcdb03fe30df0
AErbsloeh <andreas.erbsloeh@uni-due.de>
2023-11-27 14:17:25 +0100

    Added first CNN (bad accuracy)

--- 3_Python/package/dnn/models/rgc_onoff_class.py
+++ 3_Python/package/dnn/models/rgc_onoff_class.py
@@ -86,35 +86,44 @@ class cnn_rgc_onoff_v1(nn.Module):
         self.out_modeltyp = 'Classification'
         self.model_embedded = False
         self.model_shape = (1, input_size)
-        kernel_layer = [1, 22, 8]
+        kernel_layer = [1, 6, 8]
         kernel_size = [3, 3, 3]
         kernel_stride = [1, 1, 1]
         kernel_padding = [0, 0, 0]
-        lin_size = [16, 10, output_size]
+        lin_size = [224, 140, 90, 30, output_size]
         do_train_bias = True
 
-        self.classifier = nn.Sequential(
+        self.cnn = nn.Sequential(
             nn.Conv1d(kernel_layer[0], kernel_layer[1], kernel_size[0],
                       stride=kernel_stride[0], padding=kernel_padding[0]),
             nn.BatchNorm1d(kernel_layer[1], affine=do_train_bias),
-            nn.Tanh(),
+            nn.SiLU(),
             nn.Conv1d(kernel_layer[1], kernel_layer[2], kernel_size[1],
                       stride=kernel_stride[1], padding=kernel_padding[1]),
             nn.BatchNorm1d(kernel_layer[2], affine=do_train_bias),
-            nn.Tanh(),
+            nn.SiLU(),
             # --- Linear Layer
-            nn.Flatten(start_dim=1),
+            nn.Flatten(start_dim=1)
+        )
+        self.classifier = nn.Sequential(
             nn.Linear(lin_size[0], lin_size[1], bias=do_train_bias),
             nn.BatchNorm1d(lin_size[1], affine=do_train_bias),
             nn.ReLU(),
             nn.Linear(lin_size[1], lin_size[2], bias=do_train_bias),
             nn.BatchNorm1d(lin_size[2], affine=do_train_bias),
+            nn.ReLU(),
+            nn.Linear(lin_size[2], lin_size[3], bias=do_train_bias),
+            nn.BatchNorm1d(lin_size[3], affine=do_train_bias),
+            nn.ReLU(),
+            nn.Linear(lin_size[3], lin_size[4], bias=do_train_bias),
+            nn.BatchNorm1d(lin_size[4], affine=do_train_bias),
             nn.Softmax(dim=0)
         )
 
     def forward(self, x: Tensor) -> [Tensor, Tensor]:
         x0 = unsqueeze(x, dim=1)
-        val = self.classifier(x0)
+        val = self.cnn(x0)
+        val = self.classifier(val)
         return val, argmax(val, dim=1)
 
 
