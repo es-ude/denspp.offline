02ed0b322b163ac86904ab747c056a6a8b87ea63
Karsten <karsten.appenzeller@gmail.com>
2024-02-04 15:39:28 +0100

    model moved to cpu before cla, training still on diff devs

--- 3_Python/package/dnn/pytorch_control.py
+++ 3_Python/package/dnn/pytorch_control.py
@@ -117,7 +117,7 @@ class training_pytorch:
         self._path2log = str()
         self._path2temp = str()
         self._path2config = str()
-        self.use_only_cpu = True
+        self.use_only_cpu = False
 
     def __setup_device(self) -> None:
         """Setup PyTorch for Training"""
