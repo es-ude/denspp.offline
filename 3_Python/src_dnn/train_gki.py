9d278dcffb12c5805d6fdbb3dcbdd91e9b77d094
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2024-05-10 12:14:49 +0200

    Add DNN trainings template for GKI with automated model deployment

--- 3_Python/src_dnn/train_gki.py
+++ 3_Python/src_dnn/train_gki.py
@@ -2,34 +2,48 @@ from torch import nn
 import matplotlib.pyplot as plt
 from package.dnn.dnn_handler import dnn_handler
 from package.dnn.pytorch_handler import Config_PyTorch, Config_Dataset
-import package.dnn.models.autoencoder_dnn as models
+import package.dnn.models.models_gki as ae_models
 
 
 config_data = Config_Dataset(
     # --- Settings of Datasets
-    data_path='../2_Data/00_Merged_Datasets',
-    data_file_name='2023-05-15_Dataset01_SimDaten_Martinez2009_Sorted.mat',
+    data_path='../../2_Data/00_Merged_Datasets',
+    #data_file_name='2023-05-15_Dataset01_SimDaten_Martinez2009_Sorted.mat',
+    data_file_name='2023-06-30_Dataset03_SimDaten_Quiroga2020_Sorted.mat',
     # --- Data Augmentation
-    data_do_augmentation=False,
-    data_num_augmentation=0,
+    data_do_augmentation=True,
+    data_num_augmentation=141,
     data_do_addnoise_cluster=False,
     # --- Data Normalization
-    data_do_normalization=False,
+    data_do_normalization=True,
     data_normalization_mode='CPU',
     data_normalization_method='minmax',
     data_normalization_setting='bipolar',
     # --- Dataset Reduction
     data_do_reduce_samples_per_cluster=False,
-    data_num_samples_per_cluster=50_000,
+    data_num_samples_per_cluster=5_000,
     data_exclude_cluster=[],
     data_sel_pos=[]
 )
 
-config_train = Config_PyTorch(
+config_train_ae = Config_PyTorch(
     # --- Settings of Models/Training
-    model=models.dnn_ae_v2(),
+    model=ae_models.dnn_ae_v1(32, 12),
     loss='MSE',
     loss_fn=nn.MSELoss(),
+    optimizer='Adam',
+    num_kfold=1,
+    num_epochs=100,
+    batch_size=256,
+    data_split_ratio=0.25,
+    data_do_shuffle=True
+)
+
+config_train_cl = Config_PyTorch(
+    # --- Settings of Models/Training
+    model=ae_models.dnn_class_v1(32, 5),
+    loss='Cross Entropy Loss',
+    loss_fn=nn.CrossEntropyLoss(),
     optimizer='Adam',
     num_kfold=1,
     num_epochs=100,
@@ -42,7 +56,7 @@ config_train = Config_PyTorch(
 def do_train_ae(dnn_handler: dnn_handler, mode_ae: int, noise_std=0.05) -> None:
     """Training routine for Autoencoders
     Args:
-        dnn_handler: Handler for configurating the routine selection for train deep neural networks
+        dnn_handler: Handler for configuring the routine selection for train deep neural networks
         mode_ae: Selected model of the Autoencoder (0: normal, 1: Denoising (mean), 2: Denoising (input)) [default:0]
         noise_std: Std of the additional noise added to the input [default: 0.05]
     """
@@ -51,16 +65,16 @@ def do_train_ae(dnn_handler: dnn_handler, mode_ae: int, noise_std=0.05) -> None:
     from package.plot.plot_dnn import results_training, plot_statistic_data
 
     print("\nTrain modules of end-to-end neural signal pre-processing frame-work (DeNSPP)")
+
     use_cell_bib = not (dnn_handler.mode_cell_bib == 0)
     use_cell_mode = 0 if not use_cell_bib else dnn_handler.mode_cell_bib - 1
-
     # --- Processing: Loading dataset and Do Training
     dataset = prepare_training(settings=config_data,
                                use_cell_bib=use_cell_bib, mode_classes=use_cell_mode,
                                mode_train_ae=mode_ae, do_classification=False,
                                noise_std=noise_std)
     data_mean = dataset.frames_me
-    trainhandler = train_nn(config_train, config_data)
+    trainhandler = train_nn(config_train_ae, config_data)
     trainhandler.load_model()
     trainhandler.load_data(dataset)
     del dataset
@@ -80,6 +94,49 @@ def do_train_ae(dnn_handler: dnn_handler, mode_ae: int, noise_std=0.05) -> None:
         )
         plot_statistic_data(data_result['train_clus'], data_result['valid_clus'],
                             path2save=logsdir, cl_dict=data_result['cl_dict'])
+
+        plt.show(block=dnn_handler.do_block)
+    print("\nThe End")
+
+
+def do_train_cl(dnn_handler: dnn_handler) -> None:
+    """Training routine for classifying neural activations
+    Args:
+        dnn_handler: Handler for configurating the routine selection for train deep neural networks
+    """
+    from package.dnn.dataset.autoencoder import prepare_training
+    from package.dnn.pytorch.classifier import train_nn
+    from package.plot.plot_dnn import plot_statistic_data
+    from package.plot.plot_metric import plot_confusion, plot_loss
+
+    print("\nTrain modules of end-to-end neural signal pre-processing frame-work (DeNSPP)")
+    use_cell_bib = not (dnn_handler.mode_cell_bib == 0)
+    use_cell_mode = 0 if not use_cell_bib else dnn_handler.mode_cell_bib - 1
+
+    # ---Loading Data, Do Training and getting the results
+    dataset = prepare_training(config_data, use_cell_bib=use_cell_bib, mode_classes=use_cell_mode, do_classification=True)
+    frame_dict = dataset.frame_dict
+    num_output = len(frame_dict)
+    trainhandler = train_nn(config_train_cl, config_data)
+    trainhandler.load_model()
+    trainhandler.load_data(dataset)
+    del dataset
+    epoch_acc = trainhandler.do_training()[-1]
+
+    # --- Post-Processing: Getting data, save and plot results
+    logsdir = trainhandler.get_saving_path()
+    data_result = trainhandler.do_validation_after_training(num_output)
+    del trainhandler
+
+    # --- Plotting
+    if dnn_handler.do_plot:
+        plt.close('all')
+        # --- Plotting full model
+        plot_loss(epoch_acc, 'Acc.', path2save=logsdir)
+        plot_confusion(data_result['valid_clus'], data_result['yclus'],
+                       path2save=logsdir, cl_dict=frame_dict)
+        plot_statistic_data(data_result['train_clus'], data_result['valid_clus'],
+                            path2save=logsdir, cl_dict=frame_dict)
 
         plt.show(block=dnn_handler.do_block)
     print("\nThe End")
