ab966c4240e7acafa090bdde06e4c6d73de6fccd
AErbsloeh <andreas.erbsloeh@uni-due.de>
2024-05-29 16:00:55 +0200

    Added dataset generation for waveforms

---
+++ 3_Python/src_mem/generate_waveform_dataset.py
@@ -0,0 +1,91 @@
+import numpy as np
+from torch.utils.data import Dataset
+from package.stim.waveform_generator import WaveformGenerator
+from package.analog.dev_noise import noise_awgn
+
+
+class DatasetWFG(Dataset):
+    """Dataset for defining different waveforms for training"""
+    def __init__(self, fs: float, waveforms: np.ndarray, classes: np.ndarray,
+                 cl_names: list, do_autoencoder=False) -> None:
+        self.sampling_rate = fs
+        self.__signals = waveforms
+        self.__classes = classes
+        self.class_names = cl_names
+        self.__do_autoencoder = do_autoencoder
+
+    def __len__(self) -> int:
+        return self.__signals.shape[0]
+
+    def __getitem__(self, idx) -> [np.ndarray, np.ndarray]:
+        if self.__do_autoencoder:
+            return self.__signals[idx, :], self.__signals[idx, :]
+        else:
+            return self.__signals[idx, :], self.__classes[idx]
+
+
+def generate_dataset(selected_wfg: list, num_wfg_class: int, freq_wfg: float, sampling_rate: float,
+                     adding_noise=False, pwr_noise_db=-90.0, get_info_classes=False) -> DatasetWFG:
+    """Generating dataset
+    Args:
+        selected_wfg:       Selected types of waveforms
+        num_wfg_class:      Number of samples for each waveform class
+        freq_wfg:           Frequency of the waveform
+        sampling_rate:      Sampling rate
+        adding_noise:       Adding noise to output
+        get_info_classes:   Getting print output with available signal types
+    Returns:
+        Dataset with waveforms sample inside
+    """
+    # --- Define classes
+    wfg_generator = WaveformGenerator(sampling_rate)
+    wfg_dict = wfg_generator.get_dictionary_classes()
+
+    if get_info_classes:
+        print("\nGetting information about signal types")
+        print("\n====================================================")
+        for idx, id in enumerate(wfg_dict):
+            print(f"Class {idx:02d} = {id}")
+        print("====================================================")
+
+    t_window = 1.5 / freq_wfg
+    t_wfg = [1 / freq_wfg]
+    t_start = [0.25 / freq_wfg]
+    num_samples = len(selected_wfg) * num_wfg_class
+
+    waveforms_signals = np.zeros((num_samples, int(sampling_rate * t_window)), dtype=float)
+    waveforms_classes = np.zeros((num_samples, ), dtype=int)
+    for idx, sel_wfg in enumerate(selected_wfg):
+        for num_ite in range(0, num_wfg_class):
+            t0, signal0 = wfg_generator.generate_waveform(t_start, t_wfg, [sel_wfg], [False])
+            if adding_noise:
+                signal0 += noise_awgn(t0.size, sampling_rate, pwr_noise_db)
+
+            waveforms_signals[idx * num_wfg_class + num_ite, :] = signal0
+            waveforms_classes[idx * num_wfg_class + num_ite] = idx
+
+    waveforms_dict = list()
+    for id in np.unique(selected_wfg):
+        waveforms_dict.append(wfg_dict[id])
+
+    return DatasetWFG(
+        fs=sampling_rate,
+        waveforms=waveforms_signals,
+        classes=waveforms_classes,
+        cl_names=waveforms_dict
+    )
+
+
+if __name__ == "__main__":
+    num_wfg_class = 2
+    sel_wfg_class = [0, 5, 7, 8]
+
+    dataset = generate_dataset(sel_wfg_class, num_wfg_class, 2, 10e3, get_info_classes=True)
+    signal_type = dataset.class_names
+
+    print(f"\n======================================================")
+    for idx, data in enumerate(dataset):
+        print(f"Sample #{idx:03d} ({signal_type[data[1]]}: {data}")
+    print(f"\n======================================================")
+    print("Done")
+
