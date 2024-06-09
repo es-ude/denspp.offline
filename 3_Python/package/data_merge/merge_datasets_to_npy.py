58701c82c9f8d83da3481c7ef35eeed309dbcce4
HarisAdrovic <Haris.Adrovic1@icloud.com>
2024-06-09 10:22:37 +0200

    Added Klaesfile merging to a single array with dimension of ((783830, 48) (Number of waveforms x samples per waveform)

--- 3_Python/package/data_merge/merge_datasets_to_npy.py
+++ 3_Python/package/data_merge/merge_datasets_to_npy.py
@@ -5,12 +5,11 @@ import numpy
 
 from scipy.io import loadmat
 
+
 class DataCompressor:
 
     def __init__(self, data_type):
-        self.filepath= r"C:\Users\Haris\Documents\Master Sciebo neu\SS2024\CPS Projekt1\2024-02-05_Dataset-KlaesNeuralDecoding.mat"
-
-
+        self.filepath = r"C:\Users\Haris\Documents\Master Sciebo neu\SS2024\CPS Projekt1\2024-02-05_Dataset-KlaesNeuralDecoding.mat"
 
         self.mode_data = data_type
         if data_type == 1:
@@ -36,37 +35,59 @@ class DataCompressor:
         loaded_data = loadmat(self.filepath)
         return loaded_data
 
-    def format_data_klaes(self):
+    def format_data(self):
+
+        exp_index = 0
         trial_index = 0
-        trial_index = 0
-        loaded_data = self.load_Data()
-        b = self.create_Dict()
 
-        ##format data
-        for exp_key in loaded_data:
+        if self.data_type == "Klaes-file":
 
-            if exp_key.startswith("exp"):
+            loaded_data = self.load_Data()
+            b = self.create_Dict()
 
-                exp_data = loaded_data[exp_key]
+            ##format data
+            for exp_key in loaded_data:
 
-                for trial_key in exp_data.dtype.names:
-                    if trial_key.startswith("trial"):
+                if exp_key.startswith("exp"):
 
-                        trial_data = exp_data[trial_key][0, 0]
-                        waveforms = trial_data["waveforms"][0, 0]
+                    exp_data = loaded_data[exp_key]
 
-                        for electrode in range(96):
-                            b[trial_index][trial_index][electrode] = waveforms[0][electrode]
+                    for trial_key in exp_data.dtype.names:
+                        if trial_key.startswith("trial"):
 
-                    trial_index += 1
+                            trial_data = exp_data[trial_key][0, 0]
+                            waveforms = trial_data["waveforms"][0, 0]
 
-                trial_index = 0
-                trial_index += 1
+                            for electrode in range(96):
+                                b[exp_index][trial_index][electrode] = waveforms[0][electrode]
 
-        numpy_array = np.array(list(b.values()))  # cast dict as list -> np.array
+                        trial_index += 1
 
-        numpy.save("_waveforms" + ".npy", numpy_array)
+                    trial_index = 0
+                    exp_index += 1
+
+            numpy_array = np.array(list(b.values()))  # cast dict as list -> np.array
+
+            a = []
+
+            waveform_index = 0
+            for x in range(len(b)):
+                for y in range(len(b[x])):
+                    for z in range(len(b[x][y])):
+                        for v in range(len(b[x][y][z])):
+                            if len(b[x][y][z][v]) == 48:
+                                # a.append([waveform_index]+ list(data[x][y][z][v])) # so steht der index vorne dran
+                                a.append(b[x][y][z][v])
+                                waveform_index += 1
+
+                            else:
+                                continue
+            a = np.array(a)
+
+            np.save("_waveforms_as_one_array" + ".npy", a)
+
+            print(a.shape)
 
 
-
-
+trialONE = DataCompressor(1)
+trialONE.format_data()
