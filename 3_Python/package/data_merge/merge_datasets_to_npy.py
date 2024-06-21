18393b9e73d0fb67cd9fa2af1c8d25f0942bc3b0
HarisAdrovic <Haris.Adrovic1@icloud.com>
2024-06-21 15:11:35 +0200

    git fix

--- 3_Python/package/data_merge/merge_datasets_to_npy.py
+++ 3_Python/package/data_merge/merge_datasets_to_npy.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 from scipy.io import loadmat
@@ -8,11 +7,10 @@ class DataCompressor:
 
     def __init__(self, data_type):
 
-
         self.mode_data = data_type
         if data_type == 1:
             self.data_type = "Klaes-file"
-            self.filepath = r"C:\Users\Haris\Documents\Master Sciebo neu\SS2024\CPS Projekt1\2024-02-05_Dataset-KlaesNeuralDecoding.mat"
+            self.filepath = r"C:\Users\Haris\Documents\Master Sciebo neu\SS2024\CPS Projekt1\old\2024-02-05_Dataset-KlaesNeuralDecoding.mat"
             self.num_experiments = 21
             self.num_trials = 50
         elif data_type == 2:
@@ -67,9 +65,10 @@ class DataCompressor:
                     trial_index = 0
                     exp_index += 1
 
-
             ### format to one 2D array
             a = []
+            lowestindex = []
+            highestindex =[]
             waveform_index = 0
             for x in range(len(b)):
                 for y in range(len(b[x])):
@@ -77,21 +76,36 @@ class DataCompressor:
                         for v in range(len(b[x][y][z])):
                             if len(b[x][y][z][v]) == 48:
                                 # a.append([waveform_index]+ list(data[x][y][z][v])) # so steht der index vorne dran
-                                a.append(b[x][y][z][v])
-                                #a.append(self.normalize(b[x][y][z][v], 0,1))        # so normalisiert, schlecht für kmeans
+                                # a.append(self.normalize(b[x][y][z][v], 0,1))        # so normalisiert, schlecht für kmeans
 
-                                waveform_index += 1
+                                lowestindex_current = np.argmin(b[x][y][z][v])
+                                highestindex_current = np.argmax(b[x][y][z][v])
+                                if lowestindex_current == 11 or lowestindex_current == 12 or lowestindex_current == 13:
+                                    if highestindex_current < 1 or highestindex_current >13:
+                                        a.append(b[x][y][z][v])
+                                        highestindex.append(highestindex_current)
+                                        lowestindex.append(lowestindex_current)
+                                        waveform_index += 1
 
                             else:
                                 continue
             a = np.array(a)
 
-            np.save("_waveforms_as_one_array" + ".npy", a)
+            np.save("_waveforms_as_one_array_cleaned" + ".npy", a)
+            np.save("lowest_index_of_waveforms" + ".npy", lowestindex)
+            np.save("highest_index_of_waveforms" + ".npy", highestindex)
 
             print(a.shape)
 
 
 trialONE = DataCompressor(1)
 trialONE.format_data()
-a = np.load(r"C:\Users\Haris\git\CPS_Projekt\denspp.offline\3_Python\package\data_merge\_waveforms_as_one_array.npy")
-print(a[0])
+a = np.load(
+    r"C:\Users\Haris\git\CPS_Projekt\denspp.offline\3_Python\package\data_merge\_waveforms_as_one_array_cleaned.npy")
+b = np.load(r"C:\Users\Haris\git\CPS_Projekt\denspp.offline\3_Python\package\data_merge\lowest_index_of_waveforms.npy")
+c = np.load(r"C:\Users\Haris\git\CPS_Projekt\denspp.offline\3_Python\package\data_merge\highest_index_of_waveforms.npy")
+print(b)
+counts = np.bincount(c)
+print(counts)
+for number, count in enumerate(counts):
+    print(f"Zahl {number} kommt {count / 783830} Mal vor.")
