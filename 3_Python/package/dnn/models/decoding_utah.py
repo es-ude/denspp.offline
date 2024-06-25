40f38b13d7e0d1fb448d9e4df4a6f49601a5d65d
Jannik Graf <muskelgurke@gmx.net>
2024-06-25 11:45:57 +0200

    V3 cnn2d_lstm_dec_v3 hinzugefügt

--- 3_Python/package/dnn/models/decoding_utah.py
+++ 3_Python/package/dnn/models/decoding_utah.py
@@ -2,6 +2,73 @@ from torch import nn, Tensor, argmax
 import torch
 from package.dnn.pytorch_handler import Config_PyTorch, Config_Dataset
 
+class cnn2D_lstm_dec_v3(nn.Module):
+    """Class of a convolutional Decoding for feature extraction"""
+    def __init__(self, num_clusters=1, input_samples=12, output_samples=3):
+        super().__init__()
+        self.out_modelname = 'cnn_lstm_dec_v1'
+        self.out_modeltyp = 'Decoder'
+        self.model_embedded = False
+        self.model_shape = (1, num_clusters, 10, 10, input_samples)
+        do_bias_train = True
+
+        kernel_layer = [num_clusters, 10, 20]
+        # --- Settings for DNN/LSTM
+        dense_layer_size = [64, 32]
+
+        self.cnn_1 = nn.Sequential(
+            nn.Conv2d(in_channels = num_clusters,
+                      out_channels= kernel_layer[1],
+                      kernel_size=3,
+                      stride= 1,
+                      padding= 0),
+            nn.BatchNorm2d(num_features= kernel_layer[1]),
+            nn.ReLU(),
+        )
+
+        self.dnn_1 = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(
+                in_features= dense_layer_size[0],
+                out_features= dense_layer_size[1],
+                bias=do_bias_train
+            ),
+            nn.BatchNorm1d(dense_layer_size[1]),
+            nn.ReLU(),
+            nn.Linear(
+                in_features= dense_layer_size[1],
+                out_features= output_samples,
+                bias=do_bias_train
+            ),
+            nn.BatchNorm1d(output_samples),
+            nn.Softmax()
+        )
+
+        self.flatten = nn.Flatten()
+        self.lstm = nn.LSTM(
+            input_size=640,  # Adjust based on the output size from CNN
+            hidden_size=64,          # Example hidden size, adjust as needed
+            num_layers=input_samples,            # Example number of LSTM layers, adjust as needed
+            batch_first=True         # Input and output tensors are provided as (batch, seq, feature)
+        )
+
+    def forward(self, x):
+        batch_size, num_clusters, height, width, num_time_windows = x.shape
+        video = []
+        #print("debug <3")
+        for i in range(num_time_windows):
+            img = x[:, :, :, :, i]
+            img.view(batch_size, num_clusters, 10, 10) #batchsize ist immer 1
+
+            cnn_out = self.cnn_1(img)
+            cnn_out = cnn_out.view(batch_size, -1)  # Flatten
+            video.append(cnn_out.unsqueeze(1))  # Add time dimension
+        lstm_input = torch.cat(video, dim=1)  # Concatenate along the time dimension
+        lstm_output, _ = self.lstm(lstm_input)
+        pred_con = self.dnn_1(lstm_output[:,-1,:])#get last lstm output
+        #print("debug <3")
+
+        return pred_con, argmax(pred_con, 1)
 class cnn2D_lstm_dec_v2(nn.Module):
     """Class of a convolutional Decoding for feature extraction"""
     def __init__(self, num_clusters=1, input_samples=12, output_samples=3):
