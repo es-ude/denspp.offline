4ab650048debe9d6a8ea144c3b991f2575ad30b6
AErbsloeh <andreas.erbsloeh@uni-due.de>
2024-08-05 14:06:48 +0200

    Add YAML functions for writing and reading

--- 3_Python/package/structure_builder.py
+++ 3_Python/package/structure_builder.py
@@ -1,7 +1,38 @@
+import yaml
 from os import mkdir, getcwd
 from os.path import join, exists
 from glob import glob
 from shutil import copy
+
+
+def write_data_to_yaml_file(config_data: dict, filename: str, path2save='') -> None:
+    """Writing list with configuration sets to YAML file
+    Args:
+        config_data:    Dict. with configuration
+        filename:       YAML filename
+        path2save:      Optional setting for destination to save
+    Returns:
+        None
+    """
+    path2yaml = join(path2save, f'{filename}.yaml')
+    with open(path2yaml, 'w') as f:
+        yaml.dump(config_data, f, sort_keys=False)
+
+
+def read_yaml_data_to_data(filename: str, path2save='') -> dict:
+    """Writing list with configuration sets to YAML file
+    Args:
+        filename:       YAML filename
+        path2save:      Optional setting for destination to save
+    Returns:
+        Dict. with configuration
+    """
+    out_dict = dict()
+    path2yaml = join(path2save, f'{filename}.yaml')
+
+    with open(path2yaml, 'r') as f:
+        out_dict = yaml.safe_load(f)
+    return out_dict
 
 
 def _create_folder_general_firstrun() -> None:
@@ -41,3 +72,22 @@ def _create_folder_dnn_firstrun() -> None:
             else:
                 copy(file, f"{path2dst}/")
         print("Please restart the training routine!")
+
+
+if __name__ == "__main__":
+    _create_folder_general_firstrun()
+    _create_folder_dnn_firstrun()
+
+    data_wr = {
+        'Name': 'John Doe',
+        'Position': 'DevOps Engineer',
+        'Location': 'England',
+        'Age': '26',
+        'Experience': {'GitHub': 'Software Engineer', 'Google': 'Technical Engineer', 'Linkedin': 'Data Analyst'},
+        'Languages': {'Markup': ['HTML'], 'Programming': ['Python', 'JavaScript', 'Golang']}
+    }
+
+    yaml_output = yaml.dump(data_wr, sort_keys=False)
+    write_data_to_yaml_file(data_wr, 'output')
+    data_rd = read_yaml_data_to_data('output')
+    print(f"Is content of dict equal?: {data_wr == data_rd}")
