006c8f8e64b36a0432957b5ee01ba6e622bcd516
AErbsloeh <andreas.erbsloeh@uni-due.de>
2024-08-08 15:35:26 +0200

    Added yaml config handler for reading and writing yaml files

--- 3_Python/package/structure_builder.py
+++ 3_Python/package/structure_builder.py
@@ -1,43 +1,8 @@
 import yaml
-from os import mkdir, getcwd, remove
+from os import mkdir, getcwd
 from os.path import join, exists
 from glob import glob
 from shutil import copy
-
-
-def write_data_to_yaml_file(config_data: dict, filename: str, path2save='', print_output=False) -> None:
-    """Writing list with configuration sets to YAML file
-    Args:
-        config_data:    Dict. with configuration
-        filename:       YAML filename
-        path2save:      Optional setting for destination to save
-    Returns:
-        None
-    """
-    path2yaml = join(path2save, f'{filename}.yaml')
-    with open(path2yaml, 'w') as f:
-        yaml.dump(config_data, f, sort_keys=False)
-
-    # --- Do output
-    if print_output:
-        yaml_output = yaml.dump(config_data, sort_keys=False)
-        print(yaml_output)
-
-
-def read_yaml_data_to_data(filename: str, path2save='') -> dict:
-    """Writing list with configuration sets to YAML file
-    Args:
-        filename:       YAML filename
-        path2save:      Optional setting for destination to save
-    Returns:
-        Dict. with configuration
-    """
-    out_dict = dict()
-    path2yaml = join(path2save, f'{filename}.yaml')
-
-    with open(path2yaml, 'r') as f:
-        out_dict = yaml.safe_load(f)
-    return out_dict
 
 
 def _create_folder_general_firstrun() -> None:
@@ -78,3 +43,83 @@ def _create_folder_dnn_firstrun() -> None:
             else:
                 copy(file, f"{path2dst}/")
         print("Please restart the training routine!")
+
+
+def write_data_to_yaml_file(config_data: dict, filename: str,
+                            path2save='', print_output=False) -> None:
+    """Writing list with configuration sets to YAML file
+    Args:
+        config_data:    Dict. with configuration
+        filename:       YAML filename
+        path2save:      Optional setting for destination to save
+        print_output:   Printing the data in YAML format
+    Returns:
+        None
+    """
+    path2yaml = join(path2save, f'{filename}.yaml')
+    with open(path2yaml, 'w') as f:
+        yaml.dump(config_data, f, sort_keys=False)
+
+    # --- Do output
+    if print_output:
+        yaml_output = yaml.dump(config_data, sort_keys=False)
+        print(yaml_output)
+
+
+def read_yaml_data_to_data(filename: str, path2save='',
+                           print_output=False) -> dict:
+    """Writing list with configuration sets to YAML file
+    Args:
+        filename:       YAML filename
+        path2save:      Optional setting for destination to save
+        print_output:   Printing the data in YAML format
+    Returns:
+        Dict. with configuration
+    """
+    out_dict = dict()
+    path2yaml = join(path2save, f'{filename}.yaml')
+    if not exists(path2yaml):
+        print("YAML does not exists - Please create one!")
+
+    with open(path2yaml, 'r') as f:
+        out_dict = yaml.safe_load(f)
+    return out_dict
+
+
+class yaml_config_handler:
+    __path2yaml: str
+    __yaml_name: str
+    _data: dict
+
+    @property
+    def path2chck(self) -> str:
+        return join(self.__path2yaml, f"{self.__yaml_name}.yaml")
+
+    def __init__(self, dummy_yaml: dict, path2yaml='', yaml_name='Config_Train'):
+        """"""
+        self.__path2yaml = path2yaml
+        self.__yaml_name = yaml_name
+
+        if not exists(self.path2chck):
+            write_data_to_yaml_file(
+                dummy_yaml,
+                self.__yaml_name, self.__path2yaml
+            )
+            print("... created new yaml file in folder!")
+
+        self._data = {}
+        self._data = read_yaml_data_to_data(
+            self.__yaml_name,
+            self.__path2yaml
+        )
+
+    def list_keys(self) -> None:
+        """"""
+        print("\nPrinting the keys and values of existing data")
+        print("=======================================================")
+        for key in self._data.keys():
+            print(key)
+
+    def get_value(self, param: str):
+        """"""
+        return self._data[param]
