1071a39ef1839ec48aaf072f9cd0e651527a4162
AErbsloeh <andreas.erbsloeh@uni-due.de>
2024-10-29 23:31:09 +0100

    feature(fpga): Added delay line or FIR allpass filter code for verilog

---
+++ 3_Python/package/fpga/template_verilog/filter_fir_allpass_template.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.11.2020 14:08:53
+// Design Name:     UDE-ES, AE
+// Module Name:     FIR (OneMultiplier)
+// Project Name: 
+// Target Devices:  ASIC (Implementing and using the mutArrayS module)
+//                  FPGA (Entfernen des mutArray und Einsetzen des DSP-Blocks)
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:        
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+// Company:         University of Duisburg-Essen, Intelligent Embedded Systems Lab
+// Engineer:        AE
+//
+// Create Date:     22.10.2024 14:16:08
+// Copied on: 	    {$date_copy_created}
+// Module Name:     FIR All-pass Filter
+// Target Devices:  ASIC
+//                  FPGA
+// Tool Versions:   1v0
+// Description:     Performing a FIR all-pass filtering on FPGA with custom made filter coefficients
+// Processing:      Data applied on posedge clk
+// Dependencies:    None
+//
+// State: 	        Works! (System Test done: 29.10.2024 on Arty A7-35T with 20% usage)
+// Improvements:    None
+// Parameters:      BITWIDTH_DATA --> Bitwidth of input data
+//                  LENGTH --> Length of used taps (=FIR filter order)
+//////////////////////////////////////////////////////////////////////////////////
+
+// Used taps order of {$filter_order} @ a sampling rate of {sampling_rate} Hz;
+module Filter_FIR_ALL_{$device_id}#(
+    parameter BITWIDTH_DATA = 6'd{$bitwidth_data},
+    parameter LENGTH = 10'd{$filter_order}
+)(
+    input wire CLK,
+    input wire nRST,
+    input wire EN,
+    input wire START_FLAG,
+    input wire [BITWIDTH_DATA-'d1:0] DATA_IN,
+    output wire [BITWIDTH_DATA-'d1:0] DATA_OUT,
+    output wire DATA_VALID
+);
+    localparam STATE_IDLE = 1'd0, STATE_PREP = 1'd1;
+    
+    //Control Signals
+    reg state;
+    reg [$clog2(LENGTH):0] cnt_adr_data;
+    reg [BITWIDTH_DATA-'d1:0] taps_fir [LENGTH:0];
+       
+    assign DATA_VALID = (state == STATE_IDLE) && EN; 
+    assign DATA_OUT = taps_fir[cnt_adr_data];
+        
+    //Performing FIR allpass filtering or delay line
+    integer i0;
+    always@(posedge CLK) begin
+        if(~(nRST && EN)) begin
+            state <= STATE_IDLE;
+            cnt_adr_data <= 'd0;
+            for(i0 = 0; i0 < LENGTH; i0 = i0 + 'd1) begin
+                taps_fir[i0] = 'd0;
+            end
+        end else begin
+            case(state)
+                STATE_IDLE: begin
+                    state <= (START_FLAG) ? STATE_PREP : STATE_IDLE;
+                end
+                STATE_PREP: begin
+                    taps_fir[cnt_adr_data] <= DATA_IN;
+                    state <= STATE_IDLE;
+                    if(cnt_adr_data == LENGTH-'d1) begin
+                        cnt_adr_data <= 'd0;
+                    end else begin
+                        cnt_adr_data <= cnt_adr_data + 'd1;
+                    end
+                end
+            endcase
+        end
+    end
+endmodule
