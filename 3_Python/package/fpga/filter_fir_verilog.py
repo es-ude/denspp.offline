f784c487da2d9bd5f7d314b462268c6afaa70e2e
AErbsloeh <andreas.erbsloeh@uni-due.de>
2024-10-29 22:30:12 +0100

    refractored(fpga): FIR filter generation (tested on FPGA)

--- 3_Python/package/fpga/filter_fir_verilog.py
+++ 3_Python/package/fpga/filter_fir_verilog.py
@@ -2,35 +2,37 @@ from os import mkdir, getcwd
 from os.path import join, isdir
 from numpy import log2, ceil
 from datetime import datetime
-from fxpmath import Fxp
 
 from package.fpga.helper.emulator_filter import filter_stage
 from package.fpga.helper.translater import replace_variables_with_parameters, read_template_design_file
 
 
-def generate_fir_filter_files(data_bitsize: int, data_bitfrac: int, data_signed: bool,
+def generate_fir_filter_files(data_bitsize: int, data_signed: bool,
                               filter_order: int, sampling_rate: float, filter_corner: list,
-                              module_id='0', filter_btype='low', filter_ftype='butter', use_ram_coeff=False,
-                              do_optimized=False, file_name='filter_fir', path2save='') -> None:
+                              module_id='0', filter_btype='low', filter_ftype='butter', use_ext_coeff=False,
+                              do_optimized=False, weights_bitsize=0, file_name='filter_fir', path2save='') -> None:
     """Generating Verilog files for FIR filtering on FPGAs/ASICs
+    (Fraction of all weights have data_bitsize-2)
     Args:
-        data_bitsize:   Used quantization level for data stream
-        data_bitfrac:   Fraction size of used bitwidth
-        data_signed:    Decision if data stream in-/output values are signed [otherwise unsigned]
-        filter_order:   Order of the filter
-        sampling_rate:  Sampling clock of data stream processing
-        filter_corner:  List with corner frequency of the used filter
-        module_id:      ID of used filter structure
-        filter_btype:   Used filter type ['low', 'high', 'bandpass', 'bandstop']
-        filter_ftype:   Used filter design ['butter', 'cheby1', 'cheby2', 'ellip', 'bessel']
-        use_ram_coeff:  Using a RAM for saving the filter coefficients inside
-        do_optimized:   Decision if LUT resources should be minimized [only quarter and mirroring]
-        file_name:      Name of the generated files
-        path2save:      Path for saving the verilog output files
+        data_bitsize:       Used quantization level for data stream
+        data_signed:        Decision if data stream in-/output values are signed [otherwise unsigned]
+        filter_order:       Order of the filter
+        sampling_rate:      Sampling clock of data stream processing
+        filter_corner:      List with corner frequency of the used filter
+        module_id:          ID of used filter structure
+        filter_btype:       Used filter type ['low', 'high', 'bandpass', 'bandstop']
+        filter_ftype:       Used filter design ['butter', 'cheby1', 'cheby2', 'ellip', 'bessel']
+        use_ext_coeff:      Using an external RAM for saving the filter coefficients inside
+        do_optimized:       Decision if LUT resources should be minimized [only quarter and mirroring]
+        weights_bitsize:    Bitsize of all weights [Default: 0 --> data_bitsize]
+        file_name:          Name of the generated files
+        path2save:          Path for saving the verilog output files
     Return:
         None
     """
     use_dsp_slice_fpga = True
+    use_ext_mult = False
+
     if do_optimized and filter_order % 2 == 0:
         raise NotImplementedError("Please add an odd number to filter order!")
 
@@ -42,7 +44,7 @@ def generate_fir_filter_files(data_bitsize: int, data_bitfrac: int, data_signed:
     # --- Reading the filter coefficients
     filter_emulator = filter_stage(filter_order, sampling_rate, filter_corner, False,
                                    ftype=filter_ftype, btype=filter_btype)
-    filter_coeff = filter_emulator.get_coeff_quant(data_bitsize, data_bitfrac, data_signed)
+    filter_coeff = filter_emulator.get_coeff_quant(data_bitsize, data_bitsize, True)
     filter_coeff_used = filter_coeff['coeffb'] if not do_optimized else filter_coeff['coeffb'][:int(filter_order/2)+1]
 
     coeff_data_string = ''
@@ -52,13 +54,14 @@ def generate_fir_filter_files(data_bitsize: int, data_bitfrac: int, data_signed:
     # --- Definition of used parameters and replacement
     params = {
         'date_copy_created':     datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-        'use_int_weights':      '' if use_ram_coeff else '//',
-        'use_own_multiplier':   '' if not use_dsp_slice_fpga else '//',
+        'use_int_weights':      '' if not use_ext_coeff else '//',
+        'use_ext_mult':         '' if use_ext_mult else '//',
+        'use_lut_mult':         '' if not use_dsp_slice_fpga else '//',
         'device_id':            str(module_id),
         'bitwidth_data':        str(data_bitsize),
-        'bitwidth_frac':        str(data_bitfrac),
+        'bitwidth_weights':     str(data_bitsize) if weights_bitsize == 0 else str(weights_bitsize),
         'signed_data':          '0' if data_signed else '1',
-        'filter_order':         str(filter_order),
+        'filter_order':         str(filter_order) if not do_optimized else str(int(filter_order/2)+1),
         'length_coeff':         str(int(filter_order/2)+1),
         'size_cnt_reg':         str(int(ceil(log2(filter_order)))),
         'filter_type':          f'{filter_btype}, {filter_ftype}',
@@ -73,7 +76,7 @@ def generate_fir_filter_files(data_bitsize: int, data_bitfrac: int, data_signed:
     if path2save and not isdir(path2save):
         mkdir(path2save)
 
-    with open(join(path2save, f'{file_name}_{module_id}.v'), 'w') as v_handler:
+    with open(join(path2save, f'{file_name}_{module_id.lower()}.v'), 'w') as v_handler:
         for line in imple_file:
             v_handler.write(line)
     v_handler.close()
@@ -82,7 +85,7 @@ def generate_fir_filter_files(data_bitsize: int, data_bitfrac: int, data_signed:
 if __name__ == '__main__':
     path2save = '../../runs'
 
-    generate_fir_filter_files(14, 10, True, 21, 1e3, [100],
-                              module_id='full', do_optimized=False, path2save=path2save)
-    generate_fir_filter_files(14, 10, True, 21, 1e3, [100],
-                              module_id='half', do_optimized=True, path2save=path2save)
+    generate_fir_filter_files(16, False, 21, 1e3, [100],
+                              module_id='FULL', do_optimized=False, path2save=path2save)
+    generate_fir_filter_files(16, False, 21, 1e3, [100],
+                              module_id='HALF', do_optimized=True, path2save=path2save)
--- 3_Python/package/fpga/template_verilog/filter_fir_half_template.v
+++ 3_Python/package/fpga/template_verilog/filter_fir_half_template.v
@@ -2,9 +2,9 @@
 // Company:         University of Duisburg-Essen, Intelligent Embedded Systems Lab
 // Engineer:        AE
 // 
-// Create Date:     28.01.2022 14:16:08
+// Create Date:     15.10.2024 14:16:08
 // Copied on: 	    {$date_copy_created}
-// Module Name:     FIR Filter (with One Multiplier)
+// Module Name:     FIR Filter (Half weights, with One Multiplier)
 // Target Devices:  ASIC (using own multiplier with //'define *_USE_OWN_MULT)
 //                  FPGA (using multiplier from DSP slice)
 // Tool Versions:   1v0
@@ -12,56 +12,77 @@
 // Processing:      Data applied on posedge clk --> sampling on negedge clk
 // Dependencies:    mutArrayS if using own multiplier
 //
-// State: 	        Works! (System Test done: 27.04.2023)
-// Improvements:    Verbesserung des Timings: Aktuell wird das Timing Problem mit der zusaetzlichen Warteschleife behoben
+// State: 	        Works! (System Test done: 29.10.2024 on Arty A7-35T with 20% usage)
+// Improvements:    None
+// Parameters:      BITWIDTH_DATA --> Bitwidth of input data
+//                  LENGTH --> Length of used taps (=FIR filter order)
+//                  uint_io --> Do type conversion (1'b1) from unsigned to signed to unsigned else don't do
+//                  BITWIDTH_WEIGHTS --> Bitwidth of all weights (fraction is bitwidth)
 //////////////////////////////////////////////////////////////////////////////////
 {$use_int_weights}`define FIR_{$device_id}_USE_RAM_WEIGHTS
-{$use_own_multiplier}`define FIR_{$device_id}_USE_OWN_MULT
+{$use_lut_mult}`define FIR_{$device_id}_USE_LUT_MULT
+{$use_ext_mult}`define FIR_{$device_id}_USE_EXT_MULT
 
 
-module Filter_FIR_{$device_id}#(parameter BIT_WIDTH = 5'd{$bitwidth_data}, parameter BIT_FRAC = 5'd{$bitwidth_frac}, parameter LENGTH = 9'd{$filter_order}, parameter uint_io = 1'b{$signed_data})(
-    input CLK,
-    input nRST,
-    input START_FLAG,
-    input [BIT_WIDTH-'d1:0] DATA_IN,
-    output [BIT_WIDTH-'d1:0] DATA_OUT,
-	`ifndef FIR_WEIGHTS_INT
-		input signed wire [LENGTH * BIT_WIDTH-'d1:0] FILT_WEIGHTS;
+// Input values are integer or unsigned with size of BITWIDTH_DATA (no fixed point)
+// Internal operation with signed values and all weights have fraction width of BITWIDTH_DATA;
+module Filter_FIR_{$device_id}#(
+	parameter BITWIDTH_DATA = 6'd{$bitwidth_data}, 
+	parameter LENGTH = 9'd{$filter_order}, 
+	parameter uint_io = 1'b{$signed_data},
+	parameter BITWIDTH_WEIGHTS = 6'd{$bitwidth_weights}
+)(
+    input wire CLK,
+    input wire nRST,
+    input wire EN,
+    input wire START_FLAG,
+    input wire [BITWIDTH_DATA-'d1:0] DATA_IN,
+    output wire [BITWIDTH_DATA-'d1:0] DATA_OUT,
+	`ifndef FIR_{$device_id}_USE_RAM_WEIGHTS
+		input wire signed [LENGTH * BITWIDTH_WEIGHTS-'d1:0] FILT_WEIGHTS,
 	`endif
-    output FIR_RDY
+	`ifdef FIR_{$device_id}_USE_EXT_MULT
+        output wire [BITWIDTH_DATA-'d1:0] MULT_INA,
+        output wire [BITWIDTH_WEIGHTS-'d1:0] MULT_INB,
+        input wire [BITWIDTH_DATA+BITWIDTH_WEIGHTS-'d1:0] MULT_OUT,
+    `endif
+    output DATA_VALID
 );
-    localparam UPPER_MASK = BIT_WIDTH + BIT_FRAC - 5'd1;
-    localparam SIZE_CNT = 8'd{$size_cnt_reg};
-    localparam CNT_COEFF_LENGTH = 12'd{$length_coeff};
-    localparam STATE_IDLE = 3'd0, STATE_PREP = 3'd1, STATE_FIR_FORW = 3'd2, STATE_FIR_BACK = 3'd3, STATE_SHIFT = 3'd4;
+    localparam UPPER_MASK = BITWIDTH_DATA + BITWIDTH_WEIGHTS;
+    localparam STATE_IDLE = 2'b00, STATE_PREP = 2'b01, STATE_FIR_FORW = 2'b10, STATE_FIR_BACK = 2'b11;
     
     //Control Signals
-    reg cnt;
-    reg [2:0] state;
-    reg [SIZE_CNT-'d1:0] cnt_adr;
-    reg signed [BIT_WIDTH-'d1:0] taps_fir [LENGTH-'d2:0];
-    reg [2*BIT_WIDTH-'d1:0] pre_out;    
-    wire [BIT_WIDTH-'d1:0] data_in0;
-    wire signed [BIT_WIDTH-'d1:0] coeff_b [LENGTH-'d1:0];
-    wire signed [BIT_WIDTH-'d1:0] mul_ina, mul_inb;
+    reg [1:0] state;
+    reg [$clog2(LENGTH)-'d1:0] cnt_adr_wght;
+    reg [$clog2(2*LENGTH-'d1)-'d1:0] cnt_adr_data;
+    reg signed [BITWIDTH_DATA-'d1:0] taps_fir [2*LENGTH-'d2:0];
+    reg [BITWIDTH_DATA+BITWIDTH_WEIGHTS-'d1:0] pre_out;
     
-    assign FIR_RDY = (state == STATE_IDLE);    
+    wire [BITWIDTH_DATA-'d1:0] data_in0;
+    wire signed [BITWIDTH_WEIGHTS-'d1:0] coeff_b [LENGTH-'d1:0];
+    wire signed [BITWIDTH_DATA-'d1:0] mul_ina;
+    wire signed [BITWIDTH_WEIGHTS-'d1:0] mul_inb;
+    
+    assign DATA_VALID = (state == STATE_IDLE) && EN;    
     //Transfer from unsigned to signed
-    assign data_in0 = {uint_io ^ DATA_IN[BIT_WIDTH-'d1], DATA_IN[BIT_WIDTH-'d2:0]};
-    assign DATA_OUT = {uint_io ^ pre_out[UPPER_MASK], pre_out[(UPPER_MASK-'d1)-:BIT_WIDTH-'d1]};   
-    
+    assign data_in0 = {uint_io ^ DATA_IN[BITWIDTH_DATA-'d1], DATA_IN[BITWIDTH_DATA-'d2:0]};
+    assign DATA_OUT = {uint_io ^ pre_out[UPPER_MASK-'d1], pre_out[(UPPER_MASK-'d2)-:BITWIDTH_DATA-'d1]};   
     //One Clock - Multiplier
-    assign mul_ina = (state == STATE_FIR) ? ((cnt_adr == 'd0) ? data_in0 : taps_fir[cnt_adr-'d1]) : 'd0;
-    assign mul_inb = (state == STATE_FIR) ? coeff_b[cnt_adr] : 'd0;
+    assign mul_ina = ((state == STATE_FIR_FORW) || (state == STATE_FIR_BACK)) ? taps_fir[cnt_adr_data] : 'd0;
+    assign mul_inb = ((state == STATE_FIR_FORW) || (state == STATE_FIR_BACK)) ? coeff_b[cnt_adr_wght] : 'd0;
     
     //Choicing the multiplier module
-    wire signed [2*BIT_WIDTH-'d1:0] mul_out;
-    `ifdef FIR_{$device_id}_USE_OWN_MULT
-        mutArrayS#(BIT_WIDTH) MUT(
+    wire signed [BITWIDTH_DATA+BITWIDTH_WEIGHTS-'d1:0] mul_out;
+    `ifdef FIR_{$device_id}_USE_LUT_MULT
+        mutArrayS#(BITWIDTH_DATA) MUT(
             .A(mul_ina),
             .B(mul_inb),
             .Q(mul_out)
         );
+    `elsif FIR_{$device_id}_USE_EXT_MULT
+        assign MULT_INA = mul_ina;
+        assign MULT_INB = mul_inb;
+        assign mul_out = MULT_OUT;
     `else
        assign mul_out = mul_ina * mul_inb;
     `endif
@@ -69,64 +90,72 @@ module Filter_FIR_{$device_id}#(parameter BIT_WIDTH = 5'd{$bitwidth_data}, param
     //Performing FIR
     integer i0;
     always@(posedge CLK) begin
-        if(~nRST) begin
+        if(~(nRST && EN)) begin
             state <= STATE_IDLE;
-            cnt_adr <= 'd0;
-            for(i0 = 0; i0 < LENGTH-'d1; i0 = i0 + 'd1) begin
+            cnt_adr_data <= 'd0;
+            cnt_adr_wght <= 'd0;
+            for(i0 = 0; i0 < 2*LENGTH-'d1; i0 = i0 + 'd1) begin
                 taps_fir[i0] = 'd0;
             end
-            pre_out <= 'd0;
-            cnt <= 1'd0;
         end else begin
             case(state)
                 STATE_IDLE: begin
                     state <= (START_FLAG) ? STATE_PREP : STATE_IDLE;
                 end
                 STATE_PREP: begin
-                    state <= STATE_FIR;
-                    pre_out <= 'd0;
+                    taps_fir[cnt_adr_data] <= data_in0;
+                    state <= STATE_FIR_FORW;
                 end
                 STATE_FIR_FORW: begin
-                    if(cnt_adr == CNT_COEFF_LENGTH-'d1) begin
-                        cnt_adr <= 'd0;
+                    //Cnt structure for weight counter
+                    if(cnt_adr_wght == LENGTH-'d1) begin
+                        cnt_adr_wght <= LENGTH-'d2;
                         state <= STATE_FIR_BACK;
-                        cnt <= 1'd0;
                     end else begin 
-                        cnt_adr <= (cnt == 1'd1) ? cnt_adr + 'd1 : cnt_adr;
-                        state <= (cnt == 1'd1) ? STATE_FIR_FORW : state;
-                    end
-                    pre_out <= pre_out + ((cnt == 1'd1) ? mul_out : 'd0);
-                    cnt <= ~cnt;
+                        cnt_adr_wght <= cnt_adr_wght + 'd1;
+                        state <= STATE_FIR_FORW;
+                    end 
+                    //Cnt structure for data counter
+                    if(cnt_adr_data == 'd0) begin
+                        cnt_adr_data <= 2*LENGTH-'d2;
+                    end else begin 
+                        cnt_adr_data <= cnt_adr_data - 'd1;
+                    end 
                 end
                 STATE_FIR_BACK: begin
-                    if(cnt_adr == CNT_COEFF_LENGTH-'d1) begin
-                        cnt_adr <= 'd0;
-                        state <= STATE_SHIFT;
-                        cnt <= 1'd0;
-                    end else begin
-                        cnt_adr <= (cnt == 1'd1) ? cnt_adr + 'd1 : cnt_adr;
-                        state <= (cnt == 1'd1) ? STATE_FIR_BACK : state;
+                    //Cnt structure for weight counter
+                    if(cnt_adr_wght == 'd0) begin
+                        cnt_adr_wght <= 'd0;
+                        state <= STATE_IDLE;
+                        cnt_adr_data <= cnt_adr_data;
+                    end else begin 
+                        cnt_adr_wght <= cnt_adr_wght - 'd1;
+                        state <= STATE_FIR_BACK;
+                        //Cnt structure for data counter
+                        if(cnt_adr_data == 'd0) begin
+                            cnt_adr_data <= 2*LENGTH-'d2;
+                        end else begin 
+                            cnt_adr_data <= cnt_adr_data - 'd1;
+                        end 
                     end
-                    pre_out <= pre_out + ((cnt == 1'd1) ? mul_out : 'd0);
-                    cnt <= ~cnt;
-                end
-                STATE_SHIFT: begin
-                    cnt <= 1'd0;
-                    for(i0 = 'd0; i0 < LENGTH-'d1; i0 = i0 + 'd1) begin
-                        taps_fir[i0] <= (i0 == 'd0) ? data_in0 : taps_fir[i0-'d1];
-                    end
-                    state <= STATE_IDLE;
                 end
             endcase
         end
-    end  
+    end   
+    always@(negedge CLK) begin
+        if(~nRST || (state == STATE_PREP)) begin
+            pre_out <= 'd0;
+        end else begin
+            pre_out <= pre_out + (((state == STATE_FIR_FORW) || (state == STATE_FIR_BACK)) ? mul_out : 'd0);
+        end
+    end    
     
     // Definition of the weights
 	`ifndef FIR_{$device_id}_USE_RAM_WEIGHTS
-		genvar i0;
-		for (i0 = 'd0; i0 < LENGTH; i0 = i0 + 'd1) begin
-			assign coeff_b[i0] = FILT_WEIGHTS[i0*BIT_WIDTH+:BIT_WIDTH];
-		end
+		genvar i1;
+	    for (i1 = 'd0; i1 < LENGTH; i1 = i1 + 'd1) begin
+            assign coeff_b[i1] = FILT_WEIGHTS[i1*BITWIDTH_WEIGHTS+:BITWIDTH_WEIGHTS];
+        end
     `else
 		//--- Used filter coefficients ({$filter_type}) with {$filter_corner} Hz @ {$sampling_rate} Hz
 {$coeff_data}
