5a418a183094ff74daf11f17bf162508429931ca
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-01-29 08:13:09 +0100

    refactor: changed yaml test files

--- 3_Python/package/yaml_handler.py
+++ 3_Python/package/yaml_handler.py
@@ -1,68 +1,18 @@
 import yaml
 from typing import Any
+from os import getcwd, makedirs
 from os.path import join, exists
-from package.structure_builder import get_path_project_start
-
-
-def write_dict_to_yaml(config_data: dict, filename: str,
-                       path2save='', print_output=False) -> None:
-    """Writing list with configuration sets to YAML file
-    Args:
-        config_data:    Dict. with configuration
-        filename:       YAML filename
-        path2save:      Optional setting for destination to save
-        print_output:   Printing the data in YAML format
-    Returns:
-        None
-    """
-    path2yaml = join(path2save, f'{filename}.yaml')
-    with open(path2yaml, 'w') as f:
-        yaml.dump(config_data, f, sort_keys=False)
-
-    if print_output:
-        print(yaml.dump(config_data, sort_keys=False))
-
-
-def read_yaml_to_dict(filename: str, path2save='',
-                      print_output=False) -> dict:
-    """Writing list with configuration sets to YAML file
-    Args:
-        filename:       YAML filename
-        path2save:      Optional setting for destination to save
-        print_output:   Printing the data in YAML format
-    Returns:
-        Dict. with configuration
-    """
-    path2yaml = join(path2save, f'{filename}.yaml')
-    if not exists(path2yaml):
-        print("YAML does not exists - Please create one!")
-
-    # --- Reading YAML file
-    with open(path2yaml, 'r') as f:
-        config_data = yaml.safe_load(f)
-    print(f"... read YAML file: {path2yaml}")
-
-    # --- Printing output
-    if print_output:
-        print(yaml.dump(config_data, sort_keys=False))
-    return config_data
-
-
-def translate_dataclass_to_dict(class_content: type) -> dict:
-    """Translating all class variables with default values into dict"""
-    return {key: value for key, value in class_content.__dict__.items()
-            if not key.startswith('__') and not callable(key)}
 
 
 class YamlConfigHandler:
-    __path2yaml: str
+    __path2yaml_folder: str
     __yaml_name: str
     _data: dict
 
     @property
     def path2chck(self) -> str:
         """Getting the path to the desired YAML file"""
-        return join(self.__path2yaml, f"{self.__yaml_name}.yaml")
+        return join(self.__path2yaml_folder, f"{self.__yaml_name}.yaml")
 
     def __init__(self, yaml_template: Any | dict, path2yaml='config', yaml_name='Config_Train'):
         """Creating a class for handling YAML files
@@ -70,23 +20,21 @@ class YamlConfigHandler:
             yaml_template:      Dummy dataclass with entries or dictionary (is only generated if YAML not exist)
             path2yaml:          String with path to the folder which has the YAML file [Default: '']
             yaml_name:          String with name of the YAML file [Default: 'Config_Train']
-            start_folder:       Folder to start looking for configuration folder
         """
-        self.__path2yaml = join(get_path_project_start(), path2yaml)
+        self.__path2yaml_folder = join(getcwd(), path2yaml)
         self.__yaml_name = self.__remove_ending_from_filename(yaml_name)
+        makedirs(self.__path2yaml_folder, exist_ok=True)
 
         if not exists(self.path2chck):
-            data2yaml = yaml_template if isinstance(yaml_template, dict) else translate_dataclass_to_dict(yaml_template)
-            write_dict_to_yaml(data2yaml, self.__yaml_name, self.__path2yaml)
+            data2yaml = yaml_template if isinstance(yaml_template, dict) else self.translate_dataclass_to_dict(yaml_template)
+            self.write_dict_to_yaml(data2yaml)
             print("... created new yaml file in folder!")
 
-        self._data = read_yaml_to_dict(
-            self.__yaml_name,
-            self.__path2yaml
-        )
+        self._data = self.read_yaml_to_dict()
         self.__check_scheme_validation(yaml_template, self._data)
 
-    def __remove_ending_from_filename(self, file_name: str) -> str:
+    @staticmethod
+    def __remove_ending_from_filename(file_name: str) -> str:
         """Function for removing data type ending
         :param file_name: String with file name
         :return:
@@ -107,8 +55,8 @@ class YamlConfigHandler:
         :return:
             Boolean decision if both key are equal
         """
-        template_used = translate_dataclass_to_dict(template) if not isinstance(template, dict) else template
-        real_used = translate_dataclass_to_dict(real_file) if not isinstance(real_file, dict) else real_file
+        template_used = self.translate_dataclass_to_dict(template) if not isinstance(template, dict) else template
+        real_used = self.translate_dataclass_to_dict(real_file) if not isinstance(real_file, dict) else real_file
 
         equal_chck = template_used.keys() == real_used.keys()
         if not equal_chck:
@@ -136,3 +84,43 @@ class YamlConfigHandler:
     def get_class(self, class_constructor: type):
         """Getting all key inputs from yaml dictionary to a class"""
         return class_constructor(**self._data)
+
+    def write_dict_to_yaml(self, config_data: dict, print_output: bool=False) -> None:
+        """Writing list with configuration sets to YAML file
+        Args:
+            config_data:    Dict. with configuration
+            print_output:   Printing the data in YAML format
+        Returns:
+            None
+        """
+        with open(self.path2chck, 'w') as f:
+            yaml.dump(config_data, f, sort_keys=False)
+
+        if print_output:
+            print(yaml.dump(config_data, sort_keys=False))
+
+    def read_yaml_to_dict(self, print_output: bool=False) -> dict:
+        """Writing list with configuration sets to YAML file
+        Args:
+            print_output:   Printing the data in YAML format
+        Returns:
+            Dict. with configuration
+        """
+        if not exists(self.path2chck):
+            raise FileNotFoundError("YAML does not exists - Please create one!")
+        else:
+            # --- Reading YAML file
+            with open(self.path2chck, 'r') as f:
+                config_data = yaml.safe_load(f)
+            print(f"... read YAML file: {self.path2chck}")
+
+            # --- Printing output
+            if print_output:
+                print(yaml.dump(config_data, sort_keys=False))
+            return config_data
+
+    @staticmethod
+    def translate_dataclass_to_dict(class_content: type) -> dict:
+        """Translating all class variables with default values into dict"""
+        return {key: value for key, value in class_content.__dict__.items()
+                if not key.startswith('__') and not callable(key)}
