d34c1e1cc33a2bea9a0a92cec614b2a134cb76ee
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-02-05 21:26:55 +0100

    Moved cell_bib into new files

---
+++ src_dnn/cell_bib/rgc_fzj.py
@@ -0,0 +1,20 @@
+from denspp.offline.data_call.call_cellbib import CellMergeClass
+
+RetinalGanglionCellResearchCenterJuelich = CellMergeClass(
+    cell_type_to_id = {
+        "OFF Sustained": 0,
+        "OFF Transient": 1,
+        "ON-OFF": 2,
+        "ON Sustained": 3,
+        "ON Transient": 4
+    },
+    cell_class_to_id = {},
+    cell_class_to_type = {
+        "Transient": [1, 4],
+        "Sustained": [0, 3]
+    },
+    cell_class_to_group = {
+        "OFF": [0, 1],
+        "ON": [3, 4]
+    }
+)
