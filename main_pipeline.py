c7d541284db99b93196d239eafc7e5d891bf3f0c
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-04-04 21:43:55 +0200

    Raming class names for data loading

--- main_pipeline.py
+++ main_pipeline.py
@@ -2,7 +2,7 @@ from denspp.offline.pipeline.pipeline_handler import start_pipeline_processing
 
 
 if __name__ == "__main__":
-    from src_neuro.call_spike import DataLoader
+    from src_neuro.call_data import DataLoader
     from src_neuro.pipeline_v1 import Pipeline
 
     start_pipeline_processing(DataLoader, Pipeline)
