b9f1f8d52bf3cbe7bbb5eb6292bc1cde2e82d409
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-04-25 08:50:31 +0200

    Moved tests into repo

--- run_tests.py
+++ run_tests.py
@@ -4,13 +4,13 @@ import logging
 if __name__ == '__main__':
     logger = logging.basicConfig(
         level=logging.DEBUG,
-        filename='debug_test.log',
+        filename='run_test_report.log',
         filemode='w',
         format='%(asctime)s - %(name)s - %(levelname)s = %(message)s'
     )
 
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('.', pattern='test_*.py')
+    test_suite = test_loader.discover('denspp', pattern='*_test.py')
 
     test_runner = unittest.TextTestRunner()
     test_runner.run(test_suite)
