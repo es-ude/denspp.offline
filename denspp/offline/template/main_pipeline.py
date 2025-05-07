61b89e13e3c75c7dff1ed512467857edcdc61585
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-05-07 12:57:35 +0200

    Add logger in pipeline files

--- denspp/offline/template/main_pipeline.py
+++ denspp/offline/template/main_pipeline.py
@@ -1,3 +1,4 @@
+from denspp.offline.logger import define_logger_runtime
 from denspp.offline.pipeline.pipeline_handler import start_pipeline_processing
 
 
@@ -5,4 +6,5 @@ if __name__ == "__main__":
     from src_pipe.call_data import DataLoader
     from src_pipe.pipeline_v0 import Pipeline
 
+    define_logger_runtime(False)
     start_pipeline_processing(DataLoader, Pipeline)
