04bcb706c9c193c88b5cc98e56654491bf6a11d6
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-05-07 09:35:58 +0200

    Removing application files

--- main_pipeline.py
+++
@@ -1,8 +0,0 @@
-from denspp.offline.pipeline.pipeline_handler import start_pipeline_processing
-
-
-if __name__ == "__main__":
-    from src_neuro.call_data import DataLoader
-    from src_neuro.pipeline_v1 import Pipeline
-
-    start_pipeline_processing(DataLoader, Pipeline)
