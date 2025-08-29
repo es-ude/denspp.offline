8dfcc41fe36ec710a2aec5ec0474191815f6c810
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-08-29 09:33:18 +0200

    fix(pipeline): Merged Pipeline for Merging data and normal processing into one

--- denspp/offline/template/pipeline_v0.py
+++ denspp/offline/template/pipeline_v0.py
@@ -1,33 +1,38 @@
 import numpy as np
 from os.path import abspath
 from denspp.offline.pipeline.pipeline_cmds import PipelineCMD
-from denspp.offline.analog.amplifier.pre_amp import PreAmp, SettingsAMP
-from denspp.offline.analog.adc import SettingsADC
-from denspp.offline.analog.adc.adc_sar import SuccessiveApproximation as ADC0
-from denspp.offline.preprocessing.filtering import Filtering, SettingsFilter
-from denspp.offline.preprocessing.sda import SpikeDetection, SettingsSDA
-from denspp.offline.ml.fex import FeatureExtraction, SettingsFeature
-from denspp.offline.ml.cluster import Clustering, SettingsCluster
-from denspp.offline.postprocessing.spike_analyse import calc_spiketicks
+from denspp.offline.analog import PreAmp, SettingsAMP
+from denspp.offline.analog import SettingsADC
+from denspp.offline.analog import SuccessiveApproximation as ADC0
+from denspp.offline.preprocessing import Filtering, SettingsFilter
+from denspp.offline.preprocessing import SpikeDetection, SettingsSDA
+from denspp.offline.ml import FeatureExtraction, SettingsFeature
+from denspp.offline.ml import Clustering, SettingsCluster
+from denspp.offline.postprocessing import calc_spiketicks
 from .pipeline_plot import plot_frames_feature, plot_transient_highlight_spikes, plot_transient_input_spikes
 
 
 class SettingsPipe:
-    def __overwrite_power_supply(self, vss: float, vdd: float) -> None:
+    def _overwrite_power_supply(self, vss: float, vdd: float) -> None:
         a = [method for method in dir(self) if 'Settings' in method and '__' not in method]
         for setting in a:
             set0 = getattr(self, setting)
             set0.vss = vss
             set0.vdd = vdd
 
-    def __init__(self, fs_ana: float, fs_dig: float=20e3, vss: float=-0.6, vdd: float=0.6) -> None:
+    def __init__(self, bit_adc: int, adc_dvref: float,
+                 fs_ana: float, fs_dig: float,
+                 vss: float, vdd: float
+                 ) -> None:
         """Settings class for setting-up the pipeline
-        :param fs_ana:  Sampling frequency of Analog Input [Hz]
-        :param fs_dig:  Sampling frequency of ADC output [Hz]
-        :param vss:     Negative Supply Voltage [V]
-        :param vdd:     Positive Supply Voltage [V]
+        :param bit_adc:     Bit-resolution of used ADC
+        :param adc_dvref:   Diff. voltage of ADC reference voltage [V]
+        :param fs_ana:      Sampling frequency of Analog Input [Hz]
+        :param fs_dig:      Sampling frequency of ADC output [Hz]
+        :param vss:         Negative Supply Voltage [V]
+        :param vdd:         Positive Supply Voltage [V]
         """
-        self.__overwrite_power_supply(vss, vdd)
+        self._overwrite_power_supply(vss, vdd)
 
         # --- Digital filtering for ADC output and CIC
         self.SettingsAMP = SettingsAMP(
@@ -90,7 +95,14 @@ class PipelineV0(PipelineCMD):
         self._path2pipe = abspath(__file__)
         self.generate_run_folder('runs', addon)
 
-        settings = SettingsPipe(fs_ana)
+        settings = SettingsPipe(
+            bit_adc=12,
+            adc_dvref=0.1,
+            fs_ana=fs_ana,
+            fs_dig=fs_ana,
+            vss=-0.6,
+            vdd=0.6,
+        )
         self.__preamp0 = PreAmp(settings.SettingsAMP)
         self.__adc = ADC0(settings.SettingsADC)
         self.__dsp0 = Filtering(settings.SettingsDSP_LFP)
@@ -108,10 +120,12 @@ class PipelineV0(PipelineCMD):
         plot_frames_feature(data, channel, path=self.path2save, take_feat_dim=[0, 1])
         plot_transient_highlight_spikes(data, channel, path=self.path2save, show_plot=True)
 
-    def run(self, u_in: np.ndarray) -> dict:
-        """Function with methods for emulating the end-to-end signal processing for choicen use-case
-        :param u_in:    Input signal
-        :return:        Dictionary with results
+    def run_preprocessor(self, u_in: np.ndarray, spike_xpos: list=(), spike_xoffset: int=0) -> dict:
+        """Function with methods for emulating pre-processor of the use-case-specific signal processor
+        :param u_in:            Input signal
+        :param spike_xpos:      List of all spike positions from groundtruth
+        :param spike_xoffset:   Time delay between spike_xpos and real spike
+        :return:                Dictionary with pre-processing results
         """
         # ---- Analogue Front End Module ----
         u_pre = self.__preamp0.pre_amp_chopper(u_in, np.array(self.__preamp0.vcm))['out']
@@ -120,29 +134,58 @@ class PipelineV0(PipelineCMD):
         x_lfp = self.__dsp0.filter(x_adc)
         x_spk = self.__dsp1.filter(x_adc)
         # ---- Spike detection incl. thresholding ----
-        x_dly = self.__sda.time_delay(x_spk)
-        x_sda = self.__sda.sda_spb(x_spk, [200, 2e3])
-        x_thr = self.__sda.thres_blackrock(x_sda)
-        frames_orig, frames_align = self.__sda.frame_generation(
-            x_dly, x_sda, x_thr
-        )
-        # ---- Feature Extraction | Clustering ----
-        if frames_align[1].size == 0:
-            features = np.zeros((1, ))
-            frames_align[2] = np.zeros((1, ))
-            spike_ticks = np.zeros((1, ))
+        if len(spike_xpos):
+            frames_align, frames_xpos = self.__sda.frame_generation_pos(
+                x_adc, np.array(spike_xpos), spike_xoffset
+            )[1:]
         else:
-            features = self.__fe.pca(frames_align[0])
-            frames_align[2] = self.__cl.init(features)
-            spike_ticks = calc_spiketicks(frames_align)
-
+            x_dly = self.__sda.time_delay(x_spk)
+            x_sda = self.__sda.sda_spb(x_spk, [200, 2e3])
+            x_thr = self.__sda.thres_blackrock(x_sda)
+            frames_orig, frames_align = self.__sda.frame_generation(
+                x_dly, x_sda, x_thr
+            )
         return {
             "fs_adc": self.__adc._settings.fs_adc,
             "fs_dig": self.__adc._settings.fs_dig,
             "u_in": u_in,
-            "x_adc": np.array(x_adc, dtype=np.int16),
             "x_spk": np.array(x_spk, dtype=np.int16),
-            "frames": frames_align,
-            "features": features,
-            "spike_ticks": spike_ticks
+            "frames": frames_align
         }
+
+    def run_classifier(self, data: dict) -> dict:
+        """Function with methods for emulating classifier of the use-case-specific signal processor
+        :param data:    Dictionary with pre-processed data / results
+        :return:        Dictionary with pre-processing and classification results
+        """
+        if data['frames'][1].size == 0:
+            features = np.zeros((1, ))
+            data['frames'][2] = np.zeros((1, ))
+        else:
+            features = self.__fe.pca(data['frames'][0])
+            data['frames'][2] = self.__cl.init(features)
+
+        data["features"] = features
+        return data
+
+    def run_postprocessing(self, data: dict) -> dict:
+        """Function with methods for post-processing the classified spike frames
+        :param data:    Dictionary with pre-processed data / results
+        :return:        Dictionary with pre-processing, classification and post-processed results
+        """
+        if data['frames'][1].size == 0:
+            spike_ticks = np.zeros((1,))
+        else:
+            spike_ticks = calc_spiketicks(data['frames'][1])
+
+        data["spike_ticks"] = spike_ticks
+        return data
+
+    def run(self, uin: np.ndarray) -> dict:
+        """Function with methods for emulating the end-to-end signal processing for choicen use-case
+        :param uin:     Input signal
+        :return:        Dictionary with results
+        """
+        data = self.run_preprocessor(u_in=uin)
+        data = self.run_classifier(data)
+        return self.run_postprocessing(data)
