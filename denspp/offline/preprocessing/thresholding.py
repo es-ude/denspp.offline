4031483ff8eb2a7d28fe46a45e18408baca7ac70
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-10-17 09:56:03 +0200

    fix(pre-processing): Moving downsampling methods from ADC to pre-processing

--- denspp/offline/preprocessing/thresholding.py
+++ denspp/offline/preprocessing/thresholding.py
@@ -60,8 +60,10 @@ class Thresholding:
         :return: List with names of available methods
         """
         avai_methods = [key.lower() for key in self._methods.keys()]
-        self._logger.info(f"Available Thresholding methods: {avai_methods}")
         return avai_methods
+
+    def print_overview(self) -> None:
+        self._logger.info(f"Available Thresholding methods: {self.get_overview()}")
 
     def get_threshold(self, xin: np.ndarray, do_abs: bool=False, **kwargs) -> np.ndarray:
         """Function for getting the thresholding value from input
