48714a22c07782c9ca9006e381f3585439a1ed88
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-11-05 13:25:18 +0100

    fix: Adapt logic for do_resample (removed link)
    The offset compensation was never applied to the func. Now it will be applied if necessary.

--- denspp/offline/data_call/__init__.py
+++ denspp/offline/data_call/__init__.py
@@ -1,6 +1,6 @@
 from .call_cellbib import CellSelector
 from .call_handler import SettingsData, DefaultSettingsData, DataHandler, ControllerData
-from .merge_datasets import MergeDataset, SortDataset
+from .merge_datasets import MergeDataset
 from .owncloud_handler import ConfigCloud, DefaultConfigCloud, OwnCloudDownloader
 from .waveform_dataset import SettingsWaveformDataset, DefaultSettingsWaveformDataset, WaveformGenerator
 from .waveform_generator import WaveformGenerator
