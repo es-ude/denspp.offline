169725106f15c6a78329be610fa354abf52e038d
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-11-17 14:40:27 +0100

    wip: Create file and tests for clustering metric calculation

--- denspp/offline/metric/__init__.py
+++ denspp/offline/metric/__init__.py
@@ -1,6 +1,20 @@
-from .data_numpy import (calculate_error_mbe, calculate_error_mpe, calculate_error_rae,
-                         calculate_error_rse, calculate_error_mape, calculate_error_rmse,
-                         calculate_error_rmsre, calculate_error_rrmse, calculate_error_mae, calculate_error_mse)
-from .snr import calculate_snr, calculate_snr_tensor, calculate_dsnr_tensor, calculate_snr_cluster
-from .electrical import calculate_total_harmonics_distortion, calculate_cosine_similarity
-from .timestamps import compare_timestamps
+from .data_numpy import (
+    calculate_error_mbe, calculate_error_mpe, calculate_error_rae,
+    calculate_error_rse, calculate_error_mape, calculate_error_rmse,
+    calculate_error_rmsre, calculate_error_rrmse, calculate_error_mae, calculate_error_mse
+)
+from .snr import (
+    calculate_snr, calculate_snr_tensor, calculate_dsnr_tensor, calculate_snr_cluster
+)
+from .electrical import (
+    calculate_total_harmonics_distortion, calculate_cosine_similarity
+)
+from .timestamps import (
+    compare_timestamps
+)
+from .cluster_index import (
+    calculate_harabasz,
+    calculate_silhouette,
+    calculate_dunn_index,
+    calculate_euclidean_distance
+)
