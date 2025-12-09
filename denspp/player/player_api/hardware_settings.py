6dda44b5f7ab4d538153082841f31e12d8a3f81d
Nickskil <nick.skillandat@gmx.de>
2025-12-09 21:38:24 +0100

    Generate CSV output; mapping is introduced from data channels to hardware channels; Translation value is used to calculate generic values into specific voltages.

--- denspp/player/player_api/hardware_settings.py
+++ denspp/player/player_api/hardware_settings.py
@@ -1,17 +1,18 @@
 import numpy as np
 import csv
+from dataclasses import dataclass
 
 class Hardware_settings:
     # --- Settings of used DAC
     _dac_bit: int # Bits that the DAC can handle
-    _dac_use_channel: int # Number of channels used
+    _dac_number_of_channels: int # Total number of channels the DAC channels 
     _dac_use_signed: bool # Whether the DAC uses signed values (e.g. +/- 5V or 0-10V)
     _dac_max_sampling_rate: int # Maximum sampling rate of the DAC
     _output_open: bool # Whether the output is open or 50 Ohm (needed for Oscilloscope)
     
     # --- Settings for dataset
     link_data2channel_num: list # Liste hat die Anzahl von Kanälen des DACs, es werden hier die Kanalnummern des Datensatzes angegeben, die auf den jeweiligen DAC Kanal gelegt werden sollen.
-    link_data2channel_use: list # Liste hat die Anzahl von Kanälen des DACs, gibt an ob der Kanal verwendet wird oder nicht
+
     
     # --- Settings for Groundtruth
     # Dies behinhalten exakte Spike-Zeitpunkte
@@ -20,23 +21,30 @@ class Hardware_settings:
     # Ein True am Index 3 bedeutet: "Wenn auf dem vierten Hardware-Kanal ein Signal mit Ground-Truth-Daten abgespielt wird, dann aktiviere auch den dazugehörigen digitalen Trigger-Ausgang 
 
     logger: object
-    def __init__(self, specific_device_settings: object, logger: object) -> None:
+    def __init__(self, specific_device_settings: object, logger: object, data_channel_mapping: list) -> None:
         self.logger = logger
         self._dac_bit = specific_device_settings.verticalBit
-        self._dac_use_channel = specific_device_settings.numChannels
+        self._dac_number_of_channels = specific_device_settings.numChannels
         self._dac_use_signed = specific_device_settings.usedSigned
         self._dac_max_sampling_rate = specific_device_settings.max_sampling_rate
         if hasattr(specific_device_settings, 'output_open'):
             self._output_open = specific_device_settings.output_open
+        
+        self.data_channel_mapping = data_channel_mapping
 
-        self.link_data2channel_use = [False for _ in range(self._dac_use_channel)]
-        self.link_data2channel_num = [idx for idx in range(self._dac_use_channel)]
+        self.link_data2channel_num = [False for idx in range(self._dac_number_of_channels)]
         self.groundtruth_available = False
-        self.groundtruth_activate = [False for _ in range(self._dac_use_channel)]
+        self.groundtruth_activate = [False for _ in range(self._dac_number_of_channels)]
+
+        self._set_channel_mapping()
+        self._data = None
 
     @property
-    def dac_use_channel(self):
-        return self._dac_use_channel
+    def get_data (self):
+        return self._data
+    @property
+    def dac_number_of_channels(self):
+        return self._dac_number_of_channels
 
     @property
     def dac_bit(self):
@@ -45,12 +53,20 @@ class Hardware_settings:
     @property
     def dac_use_signed(self):
         return self._dac_use_signed
+    
+    @property
+    def data_loaded(self) -> bool:
+        """Check if data is loaded
 
-    def check_active_channel(self) -> None:
-        """"""
-        for idx, ch in enumerate(self.link_data2channel_num):
-            self.link_data2channel_use[idx] = True if ch else False
-
+        Returns:
+            bool: True if data is loaded, False otherwise
+        """        
+        return True if self._data is not None else False
+    
+    def _set_channel_mapping(self) -> None:
+        """Set the mapping from data channels to hardware channels"""        
+        for data_channel, i in enumerate(self.data_channel_mapping):
+            self.link_data2channel_num[i] = data_channel
 
     def translate_data_float2int(self, data_in: list) -> list:
         # max_dac_output define the Bit depth of the DAC 12bit -> 2^12 = 4096; signed -> 2048 
@@ -81,18 +97,28 @@ class Hardware_settings:
             self.logger.info("No overflow detected in data conversion.")
         return data_out
     
-    def translate_data_for_oscilloscope(self, data_in: list) -> list:
-        """Translate data for the Oscilloscope"""
+    def translate_data_for_oscilloscope(self, resolution: float = 0.001) -> None:
+        """Translate data to the Voltage range of the Oscillioscope"""        
         max_voltage_output = 10 if self._output_open else 5 # Max voltage depending on output mode (+/-10V open, +/-5V 50 Ohm)
-        resolution = 0.001
+        
+        if self._data._translation_value_voltage is None:
+            self.logger.info("No translation value is set, using the complete voltage range for scaling")
+            max_range_use = True
+        else:
+            self.logger.info(f"Using translation value for voltage output: {self._data._translation_value_voltage}")
+            max_range_use = False
 
         data_out = list()
-        for data in data_in:
+        for data in self._data._data:
             abs_max_value_data = np.max(np.abs(data))
-            scale_factor = max_voltage_output / abs_max_value_data
             
             # Multiply each value in the signal by the scaling factor.
-            scaled_signal = data * scale_factor
+            if max_range_use:
+                scale_factor = max_voltage_output / abs_max_value_data
+                scaled_signal = data * scale_factor
+            elif not max_range_use:
+                scaled_signal = data * self._data._translation_value_voltage
+            
             scaled_signal = np.clip(scaled_signal, -max_voltage_output, max_voltage_output)
             
             # QUANTIZATION
@@ -104,64 +130,21 @@ class Hardware_settings:
 
             data_out.append(np.array(quantized_signal, dtype=np.float16))
 
-            self.logger.debug(f"Minimal Value: {np.min(quantized_signal)}, maximal Value: {np.max(quantized_signal)}")
-        return data_out
+        self._data._data = np.array(data_out, dtype=np.float16)
+        self.logger.debug(f"Minimal Value: {np.min(quantized_signal)}, maximal Value: {np.max(quantized_signal)}")
 
 
-class Board_dataset:
-    # --- Selected data to set
-    data: list #Saved the data to output to the hardware
-    samplingrate: float #Saved the sampling rate associated with the main data
-    groundtruth: list #Saved the trigger data associated with the main data
-    # --- Data and GPIO for streaming to DAC (w/o pre-processing)
-
-    def __init__(self) -> None:
-        self.data = []
-        self.samplingrate = 0.0
-        self.groundtruth = []
-    
     def create_csv_for_MXO4(self):
         """Output data in Oscilloscope MOX4 format"""
         with open('output_mox4.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
-            
-            writer.writerow([f"Rate {self.samplingrate}"])
-            for idx, data in enumerate(self.data):
-                for value in data:
-                    writer.writerow([value])
+            writer.writerow([f"Rate {self._data._samplingrate}"])
+            for data in self._data._data[self.link_data2channel_num[0]]:
+                    writer.writerow([data])
 
-
-
-
-
-def create_data_empty(point: int, dac_bit: int, dac_signed=False) -> list:
-    """
-        List with two numpy arrays
-    """
-    offset = 0 if dac_signed else 1
-    stream_data = np.zeros(shape=(point,), dtype=np.int16) + offset * np.power(2, dac_bit - 1)
-    stream_trg = np.zeros(shape=(point,), dtype=bool)
-    return stream_data, stream_trg
-
-
-def create_data_dummy_sine(point: int, dac_bit: int, dac_signed=False) -> list:
-    """
-    Return:
-        List with two numpy arrays
-    """
-    offset = 0 if dac_signed else 1
-    stream_data = np.zeros(shape=(point,), dtype=np.int16)
-    stream_trg = np.zeros(shape=(point,), dtype=bool)
-
-    # --- Apply the sinusoida waveform to output
-    for idx in range(point):
-        stream_data[idx] = np.power(2, dac_bit - 1) * (offset + 0.98 * np.sin(2 * np.pi * idx / point))
-
-    # --- Apply max peak to trigger output
-    pos = np.argwhere(stream_data == stream_data.max())
-    stream_trg[pos] = True
-    pos = np.argwhere(stream_data == stream_data.min())
-    stream_trg[pos] = True
-    
-    # --- Return data
-    return stream_data, stream_trg
+@dataclass
+class Board_dataset:
+    _data: np.ndarray #Saved the data to output to the hardware
+    _samplingrate: float #Saved the sampling rate associated with the main data
+    _groundtruth: list #Saved the trigger data associated with the main data
+    _translation_value_voltage: float # Translation value from the data points to voltage output
