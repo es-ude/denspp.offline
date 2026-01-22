467a13908a1bf05e57989ad37a147db764320d44
Nickskil <nick.skillandat@gmx.de>
2026-01-22 16:05:09 +0100

    Add a function that generates the correct CSV format for handling the denspp player SD card

--- denspp/player/data_translation.py
+++ denspp/player/data_translation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import csv
 from dataclasses import dataclass
 from logging import Logger, getLogger
@@ -5,7 +6,6 @@ from logging import Logger, getLogger
 import numpy as np
 
 from .output_devices import HardwareSpecifications
-
 
 @dataclass
 class BoardDataset:
@@ -17,11 +17,10 @@ class BoardDataset:
         groundtruth (list): Ground truth events associated with the data
         translation_value_voltage (float): Translation value from data points to voltage output
     """
-
-    data: np.ndarray  # Saved the data to output to the hardware
-    samplingrate: float  # Saved the sampling rate associated with the main data
-    groundtruth: list  # Saved the trigger data associated with the main data
-    translation_value_voltage: float  # Translation value from the data points to voltage output
+    data: np.ndarray #Saved the data to output to the hardware
+    samplingrate: float #Saved the sampling rate associated with the main data
+    groundtruth: list #Saved the trigger data associated with the main data
+    translation_value_voltage: float # Translation value from the data points to voltage output
 
 
 class DataTranslator:
@@ -59,6 +58,7 @@ class DataTranslator:
         self._set_channel_mapping(data_channel_mapping)
         self._data = None
 
+
     # ========== API METHODS ==========
     @property
     def dac_number_of_channels(self) -> int:
@@ -66,8 +66,9 @@ class DataTranslator:
 
         Returns:
             int: Number of DAC channels
-        """
+        """        
         return self._dac_number_of_channels
+
 
     @property
     def dac_bit(self) -> int:
@@ -75,8 +76,9 @@ class DataTranslator:
 
         Returns:
             int: Bit depth of the DAC
-        """
+        """        
         return self._dac_bit
+
 
     @property
     def dac_use_signed(self) -> bool:
@@ -86,6 +88,7 @@ class DataTranslator:
             bool: True if DAC uses signed values, False otherwise
         """
         return self._dac_use_signed
+    
 
     @property
     def data_loaded(self) -> bool:
@@ -93,16 +96,18 @@ class DataTranslator:
 
         Returns:
             bool: True if data is loaded, False otherwise
-        """
+        """        
         return True if self._data is not None else False
+
 
     def load_data(self, board_dataset: BoardDataset) -> None:
         """Load data into the DataTranslator
 
         Args:
             board_dataset (BoardDataset): Data to be loaded
-        """
+        """        
         self._data = board_dataset
+
 
     @property
     def get_data(self) -> BoardDataset:
@@ -114,9 +119,10 @@ class DataTranslator:
         if self._data is None:
             self._logger.error("No data loaded in DataTranslator")
             return None
-        else:
+        else:        
             return self._data
-
+        
+    
     def translation_for_device(self) -> None:
         if self._device_name == "OscilloscopeMOX4":
             self._translate_data_for_oscilloscope()
@@ -127,9 +133,10 @@ class DataTranslator:
         else:
             raise ValueError(f"data_translation: {self._device_name} not implemnented yet")
 
+
     #  ========== INTERNAL METHODS ==========
     def _set_channel_mapping(self, data_channel_mapping: list) -> None:
-        """Set the mapping from data channels to hardware channels"""
+        """Set the mapping from data channels to hardware channels"""        
         if len(data_channel_mapping) > self._dac_number_of_channels:
             self._logger.error("Data channel mapping length exceeds number of DAC channels")
             raise ValueError("Data channel mapping length exceeds number of DAC channels")
@@ -137,102 +144,92 @@ class DataTranslator:
             if data_channel_mapping[i] is not False:
                 self._link_data2channel_num[i] = data_channel_mapping[i]
 
-    def _translate_data_float2int(self, data_in: list) -> list:
-        # max_dac_output define the Bit depth of the DAC 12bit -> 2^12 = 4096; signed -> 2048
-        """"""
+
+    def _translate_data_float2int(self, min_voltage: float = -5.0, max_voltage: float= 5.) -> None:
         data_out = list()
+        for data in self._data.data:
+            transformed_data_channel = []
+            for data_point in data:
+                if data_point >= max_voltage:
+                    val0 = max_voltage * (1 - 2**-15)
+                elif data_point < min_voltage:
+                    val0 = min_voltage
+                else:
+                    val0 = data_point
+                transformed_data_channel.append(int(2**15 * (1 + val0 / max_voltage)))
+            data_out.append(np.array(transformed_data_channel, dtype=np.uint16))
+        self._data.data = np.array(data_out, dtype=np.uint16)
 
-        max_dac_output = (2**self._dac_bit) / 2 if self.dac_use_signed else (2**self._dac_bit)
-
-        for data in data_in:
-            # Find the absolute maximum value in the signal (regardless of positive or negative)
-            # With this call The loudest point in the signal (whether positive or negative) corresponds exactly to the target value max_dac_output.
-            val_abs_max = data.max() if data.max() > abs(data.min()) else data.min()
-            if val_abs_max < 0:  # Necessary otherwise the Value get inverted
-                val_abs_max = abs(val_abs_max)
-
-            scaled_data = max_dac_output / val_abs_max * data
-            if self._dac_use_signed:
-                clipped_data = np.clip(
-                    scaled_data, -32768, 32767
-                )  # Prevent overflow for signed 16-bit integer
-            else:
-                clipped_data = np.clip(
-                    scaled_data, 0, 65535
-                )  # Prevent overflow for unsigned 16-bit integer
-
-            data_out.append(np.array(clipped_data, dtype=np.int16))  # Convert to int16
-
-        if np.any(data_out[0] > 32767) or np.any(data_out[0] < -32768):
-            self._logger.warning("Overflow detected!")
-            self._logger.warning(f"Max: {np.max(data_out[0])}, Min: {np.min(data_out[0])}")
-        else:
-            self._logger.info("No overflow detected in data conversion.")
-        return data_out
 
     def _translate_data_for_oscilloscope(self, resolution: float = 0.001) -> None:
         """Translate data to the Voltage range of the Oscillioscope"""
         if self._device_name == "OscilloscopeMOX4":
-            max_voltage_output = (
-                10 if self._output_open else 5
-            )  # Max voltage depending on output mode (+/-10V open, +/-5V 50 Ohm)
+                max_voltage_output = 10 if self._output_open else 5 # Max voltage depending on output mode (+/-10V open, +/-5V 50 Ohm)
         else:
-            max_voltage_output = 5  # Default value
-
+            max_voltage_output = 5 # Default value
+        
         if self._data.translation_value_voltage is None:
             self._logger.info("No translation value is set, using the complete voltage range for scaling")
             max_range_use = True
         else:
-            self._logger.info(
-                f"Using translation value for voltage output: {self._data.translation_value_voltage}"
-            )
+            self._logger.info(f"Using translation value for voltage output: {self._data.translation_value_voltage}")
             max_range_use = False
 
         data_out = list()
         for data in self._data.data:
             abs_max_value_data = np.max(np.abs(data))
-
+            
             # Multiply each value in the signal by the scaling factor.
             if max_range_use:
                 scale_factor = max_voltage_output / abs_max_value_data
                 scaled_signal = data * scale_factor
             elif not max_range_use:
                 scaled_signal = data * self._data.translation_value_voltage
-
+            
             scaled_signal = np.clip(scaled_signal, -max_voltage_output, max_voltage_output)
-
+            
             # QUANTIZATION
-            # 1. (divide): e.g., 3.14159V / 0.001 (resolution) = 3141.59
-            # 2. (round): np.round(3141.59) = 3142
-            # 3. (multiply): 3142 * 0.001 = 3.142
-            # The result (3.142V) is a float, but it is on the 1mV grid.
+                # 1. (divide): e.g., 3.14159V / 0.001 (resolution) = 3141.59
+                # 2. (round): np.round(3141.59) = 3142
+                # 3. (multiply): 3142 * 0.001 = 3.142
+                # The result (3.142V) is a float, but it is on the 1mV grid.
             quantized_signal = np.round(scaled_signal / resolution) * resolution
 
             data_out.append(np.array(quantized_signal, dtype=np.float16))
 
         self._data.data = np.array(data_out, dtype=np.float16)
-        self._logger.debug(
-            f"Minimal Value: {np.min(quantized_signal)}, maximal Value: {np.max(quantized_signal)}"
-        )
+        self._logger.debug(f"Minimal Value: {np.min(quantized_signal)}, maximal Value: {np.max(quantized_signal)}")
+
 
     def _create_csv_for_MXO4(self) -> None:
         """Output data in Oscilloscope MOX4 format"""
-        with open("output_mox4.csv", mode="w", newline="") as file:
+        with open('output_mox4.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([f"Rate {self._data.samplingrate}"])
             for data in self._data.data[self._link_data2channel_num[0]]:
-                writer.writerow([data])
+                    writer.writerow([data])
 
+    
     def _create_csv_for_denspp_player(self) -> None:
-        with open("output_denspp_player.csv", mode="w", newline="") as file:
+        """Output data in DensPP Player format"""
+        with open('output_denspp_player.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
             if self._data.data.shape[0] < self._dac_number_of_channels:
                 for _ in range(self._dac_number_of_channels - self._data.data.shape[0]):
                     self._data.data = np.vstack([self._data.data, np.zeros(self._data.data.shape[1])])
             num_samples = self._data.data.shape[1]
             for i in range(num_samples):
-                row = [
-                    self._data.data[channel][i] if channel is not False else 0
-                    for channel in self._link_data2channel_num
-                ]
+                row = [self._data.data[channel][i] if channel is not False else 0 for channel in self._link_data2channel_num]
+                writer.writerow(row)
+
+
+    def _create_csv_for_sd_card_denspp_player(self) -> None:
+        with open("data_to_generate_denspp_player.csv", mode='w', newline='') as file:
+            writer = csv.writer(file)
+            if self._data.data.shape[0] < self._dac_number_of_channels:
+                for _ in range(self._dac_number_of_channels - self._data.data.shape[0]):
+                    self._data.data = np.vstack([self._data.data, np.zeros(self._data.data.shape[1])])
+            num_samples = self._data.data.shape[1]
+            for i in range(num_samples):
+                row = [self._data.data[channel][i] if channel is not False else 0 for channel in self._link_data2channel_num]
                 writer.writerow(row)
--- denspp/player/output_devices.py
+++ denspp/player/output_devices.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class HardwareSpecifications:
     """Hardware Settingns
-
+    
     Attributes:
         device_name (str): Name of the hardware device
         verticalBit (int): Bit depth of the DAC
@@ -13,38 +12,56 @@ class HardwareSpecifications:
         usedSigned (bool): Whether the Hardware uses signed values
         output_open (bool): If the output is open (+/-10V) or 50 Ohm (+/-5V)
     """
-
-    device_name: str
+    device_name: str 
     verticalBit: int
     numChannels: int
     max_sampling_rate: int
     usedSigned: bool
     output_open: bool
 
-
 def hardware_specification_oscilloscope_mox4() -> HardwareSpecifications:
     """Setup the hardware specifications for the Oscilloscope MOX4
 
     Returns:
         HardwareSpecifications: Hardware specifications for the Oscilloscope MOX4
-    """
+    """    
     return HardwareSpecifications(
         device_name="OscilloscopeMOX4",
         verticalBit=16,
         numChannels=1,
         max_sampling_rate=625e6,
         usedSigned=True,
-        output_open=False,
+        output_open=False
     )
 
 
 def hardware_specification_denspp_player() -> HardwareSpecifications:
+    """Setup the hardware specifications for the DensPP Player
 
+    Returns:
+        HardwareSpecifications: Hardware specifications for the DensPP Player
+    """
     return HardwareSpecifications(
         device_name="DensPPPlayer",
         verticalBit=16,
         numChannels=4,
         max_sampling_rate=1e6,
         usedSigned=True,
-        output_open=None,
+        output_open=None
+    )
+
+
+def hardware_specification_denspp_player_sdcard() -> HardwareSpecifications:
+    """Setup the hardware specifications for the DensPP Player, for the setup to safe the data on an SD Card
+
+    Returns:
+        HardwareSpecifications: Hardware specifications for the DensPP Player SD Card setup
+    """
+    return HardwareSpecifications(
+        device_name="DensPPPlayer_SDCard",
+        verticalBit=16,
+        numChannels=4,
+        max_sampling_rate=1e6,
+        usedSigned=True,
+        output_open=None
     )
