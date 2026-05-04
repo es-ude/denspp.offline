fix(metric): Add missing tests for data numpy calculation

denspp/offline/metric/data_numpy.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 
-def calculate_error_mbe(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_mbe(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with mean bias error
     Args:
         y_pred:     Numpy array or float value from prediction
         y_true:     Numpy array or float value from true label
-        do_print:   Printing the value into terminal
     Returns:
         Float value with error
     """
@@ -17,12 +16,11 @@ def calculate_error_mbe(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
         return y_pred - y_true
 
 
-def calculate_error_mae(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_mae(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with mean absolute error
     Args:
         y_pred:     Numpy array or float value from prediction
         y_true:     Numpy array or float value from true label
-        do_print:   Printing the value into terminal
     Returns:
         Float value with error
     """
@@ -33,7 +31,7 @@ def calculate_error_mae(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
         return float(np.abs(y_pred - y_true))
 
 
-def calculate_error_mse(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_mse(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with mean squared error
     Args:
         y_pred:     Numpy array or float value from prediction
@@ -48,7 +46,7 @@ def calculate_error_mse(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
         return float(y_pred - y_true) ** 2
 
 
-def calculate_error_mpe(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_mpe(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with mean percentage error
     Args:
         y_pred:     Numpy array or float value from prediction
@@ -58,13 +56,12 @@ def calculate_error_mpe(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
     """
     if isinstance(y_true, np.ndarray):
         assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
-        error = float(np.sum((y_true - y_pred) / y_true) / y_true.size)
+        return float(np.sum((y_true - y_pred) / y_true) / y_true.size)
     else:
-        error = float((y_true - y_pred) / y_true)
-    return error
+        return float((y_true - y_pred) / y_true)
 
 
-def calculate_error_mape(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_mape(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with mean absolute percentage error
     Args:
         y_pred:     Numpy array or float value from prediction
@@ -74,13 +71,12 @@ def calculate_error_mape(y_pred: np.ndarray | float, y_true: np.ndarray | float)
     """
     if isinstance(y_true, np.ndarray):
         assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
-        error = float(np.sum(np.abs(y_true - y_pred) / np.abs(y_true)) / y_true.size)
+        return float(np.sum(np.abs(y_true - y_pred) / np.abs(y_true)) / y_true.size)
     else:
-        error = float(abs(y_true - y_pred) / abs(y_true))
-    return error
+        return float(abs(y_true - y_pred) / abs(y_true))
 
 
-def calculate_error_rae(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_rae(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with relative absolute error
     Args:
         y_pred:     Numpy array or float value from prediction
@@ -91,13 +87,12 @@ def calculate_error_rae(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
     mse = calculate_error_mse(y_pred, y_true)
     if isinstance(y_pred, np.ndarray):
         assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
-        error = float(np.sqrt(mse) / calculate_error_mae(np.zeros(shape=y_pred.shape), y_true))
+        return float(np.sqrt(mse) / calculate_error_mae(np.zeros(shape=y_pred.shape), y_true))
     else:
-        error = float((mse ** 0.5) / calculate_error_mae(0.0, y_pred))
-    return error
+        return float((mse**0.5) / calculate_error_mae(0.0, y_pred))
 
 
-def calculate_error_rse(y_pred: np.ndarray | float, y_true: np.ndarray | float) -> float:
+def calculate_error_rse(y_pred: float | np.ndarray, y_true: float | np.ndarray) -> float:
     """Calculating the distance-based metric with relative squared error
     Args:
         y_pred:     Numpy array or float value from prediction
@@ -109,10 +104,9 @@ def calculate_error_rse(y_pred: np.ndarray | float, y_true: np.ndarray | float) 
     y_true_mean = np.mean(y_true)
     if isinstance(y_pred, np.ndarray):
         assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
-        error = float(mse / np.sum((y_pred - y_true_mean) ** 2) / y_pred.size)
+        return float(mse / np.sum((y_pred - y_true_mean) ** 2) / y_pred.size)
     else:
-        error = float(mse / (y_pred - y_true_mean) ** 2)
-    return error
+        return float(mse / (y_pred - y_true_mean) ** 2)
 
 
 def calculate_error_rmse(y_pred: np.ndarray, y_true: np.ndarray) -> float:
@@ -120,12 +114,11 @@ def calculate_error_rmse(y_pred: np.ndarray, y_true: np.ndarray) -> float:
     Args:
         y_pred:     Numpy array or float value from prediction
         y_true:     Numpy array or float value from true label
-        do_print:   Printing the value into terminal
     Returns:
         Float value with error
     """
     assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
-    return np.sqrt(np.sum(np.abs(y_pred - y_true) ** 2) / y_pred.size)
+    return float(np.sqrt(np.sum(np.abs(y_pred - y_true) ** 2) / y_pred.size))
 
 
 def calculate_error_rrmse(y_pred: np.ndarray, y_true: np.ndarray) -> float:
@@ -133,24 +126,22 @@ def calculate_error_rrmse(y_pred: np.ndarray, y_true: np.ndarray) -> float:
     Args:
         y_pred:     Numpy array or float value from prediction
         y_true:     Numpy array or float value from true label
-        do_print:   Printing the value into terminal
     Returns:
         Float value with error
     """
     assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
     val0 = np.sum(np.abs(y_true - y_pred) ** 2) / y_pred.size
     val1 = np.sum(np.abs(y_pred) ** 2)
-    return np.sqrt(val0/val1)
+    return float(np.sqrt(val0 / val1))
 
 
 def calculate_error_rmsre(y_pred: np.ndarray, y_true: np.ndarray) -> float:
     """Calculating the Root Mean Squared Relative ErrorArgs:
         y_pred:     Numpy array or float value from prediction
         y_true:     Numpy array or float value from true label
-        do_print:   Printing the value into terminal
     Returns:
         Float value with error
     """
     assert y_pred.shape == y_true.shape, "Dimension / shape mismatch"
     val0 = np.sum((y_true - y_pred / y_pred) ** 2)
-    return np.sqrt(val0 / y_pred.size)
+    return float(np.sqrt(val0 / y_pred.size))

denspp/offline/metric/data_numpy_test.py
@@ -1,5 +1,19 @@
 import unittest
-from denspp.offline.metric.data_numpy import *
+
+import numpy as np
+
+from denspp.offline.metric.data_numpy import (
+    calculate_error_mae,
+    calculate_error_mape,
+    calculate_error_mbe,
+    calculate_error_mpe,
+    calculate_error_mse,
+    calculate_error_rae,
+    calculate_error_rmse,
+    calculate_error_rmsre,
+    calculate_error_rrmse,
+    calculate_error_rse,
+)
 
 
 class TestErrorNumpy(unittest.TestCase):
@@ -64,8 +78,81 @@ class TestErrorNumpy(unittest.TestCase):
             y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
             y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
         )
-        chck = type(rslt) == float and rslt == 0.28133972977262783
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 0.28133972977262783)
+
+    def test_metric_mpe_float(self):
+        rslt = calculate_error_mpe(
+            y_pred=2.0,
+            y_true=-2.0,
+        )
+        chck = type(rslt) == float and rslt == 2.0
         self.assertTrue(chck)
+
+    def test_metric_mpe_numpy(self):
+        rslt = calculate_error_mpe(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 0.28133972977262783)
+
+    def test_metric_rae_float(self):
+        rslt = calculate_error_rae(
+            y_pred=2.0,
+            y_true=-2.0,
+        )
+        chck = type(rslt) == float and rslt == 2.0
+        self.assertTrue(chck)
+
+    def test_metric_rae_numpy(self):
+        rslt = calculate_error_rae(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 0.25)
+
+    def test_metric_rse_float(self):
+        rslt = calculate_error_rse(
+            y_pred=2.0,
+            y_true=-2.0,
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 1.0)
+
+    def test_metric_rse_numpy(self):
+        rslt = calculate_error_rse(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 0.0038028169014084506)
+
+    def test_metric_rmse_numpy(self):
+        rslt = calculate_error_rmse(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 1.)
+
+    def test_metric_rrmse_numpy(self):
+        rslt = calculate_error_rrmse(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 0.09699312091703453)
+
+    def test_metric_rmsre_numpy(self):
+        rslt = calculate_error_rmsre(
+            y_pred=np.linspace(1.0, 5.0, endpoint=True, num=10),
+            y_true=np.linspace(2.0, 6.0, endpoint=True, num=10),
+        )
+        self.assertEqual(type(rslt), float)
+        self.assertEqual(rslt, 3.2603112780269354)
+
 
 
 if __name__ == "__main__":