b428d04da1f82d652e60d99cffd930ea235f20df
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-05-21 11:03:29 +0200

    fix(workflow): Changed workflow for building docs

--- devenv.nix
+++ devenv.nix
@@ -10,17 +10,21 @@ in {
 
   packages = [
     pkgs.git
+    pkgs.tombi
+    pkgs.zlib # needed as dependency cocotb/ghdl under circumstances
+    pkgs.iverilog
   ];
 
   languages.c.enable = false;
   languages.nix.enable = true;
+  cachix.pull = ["nixpkgs-python"];
   languages.python = {
     enable = true;
     package = pkgs.python313;
     uv.enable = true;
-    uv.package = unstablePkgs.uv;
     uv.sync.enable = true;
     uv.sync.allExtras = true;
+    uv.package = unstablePkgs.uv;
   };
 
   processes = {
@@ -62,10 +66,45 @@ in {
     };
 
     "check:tests" = {
-        exec = ''
-             coverage run -m unittest discover -s denspp -p "*_test.py"
-             coverage report -m
-        '';
+      after = [
+        "check:slow-tests"
+        "check:fast-tests"
+      ];
+    };
+
+    "check:slow-tests" = {
+      exec = "${uv_run} --all-packages python -m pytest  -m '(simulation or slow) and not hardware'";
+      before = ["check:tests"];
+    };
+
+    "check:fast-tests" = {
+      exec = ''
+        ${uv_run} --all-packages coverage run
+        ${uv_run} coverage xml
+      '';
+      before = ["check:tests"];
+    };
+
+    "check:python-lint" = {
+      exec = "${uv_run} ruff check";
+      before = ["check:code-lint"];
+    };
+
+    "check:formatting" = {
+      exec = "${uv_run} ruff format --check";
+      before = ["check:code-lint"];
+    };
+
+    "check:toml-formatting" = {
+      exec = "${pkgs.tombi}/bin/tombi format --check";
+    };
+
+    "check:code-lint" = {
+      after = [
+        "check:nix-lint"
+        "check:python-lint"
+        "check:toml-formatting"
+      ];
     };
   };
 }
