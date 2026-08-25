56abc70f92b209f5ed7858396e6d3654f605c3bb
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-08-25 09:46:11 +0200

    feat(tests): Add instructions for deleting temp after before and after tests

--- conftest.py
+++ conftest.py
@@ -1,1 +1,22 @@
 pytest_plugins = ["elasticai.creator.testing"]
+
+
+def pytest_sessionstart(session):
+    from denspp.offline import get_path_to_project
+    from shutil import rmtree
+
+    rmtree(get_path_to_project("config"), ignore_errors=True)
+    rmtree(get_path_to_project("dataset"), ignore_errors=True)
+    rmtree(get_path_to_project("runs"), ignore_errors=True)
+    rmtree(get_path_to_project("src_dnn"), ignore_errors=True)
+    rmtree(get_path_to_project("temp_test"), ignore_errors=True)
+
+
+def pytest_sessionfinish(session, exitstatus):
+    from denspp.offline import get_path_to_project
+    from shutil import rmtree
+
+    rmtree(get_path_to_project("config"), ignore_errors=True)
+    rmtree(get_path_to_project("dataset"), ignore_errors=True)
+    rmtree(get_path_to_project("src_dnn"), ignore_errors=True)
+    rmtree(get_path_to_project("temp_test"), ignore_errors=True)
